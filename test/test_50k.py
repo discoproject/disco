00ca3a03535555eaaa4627f2087cfcccd7674df9
Ville Tuulos <tuulos@cfront.nokiapaloalto.com>
2008-04-13 00:18:35 -0700

    minor changes in test_50k test

--- test/test_50k.py
+++ test/test_50k.py
@@ -2,9 +2,8 @@
 import tserver, sys, discoapi, disco
 
 def data_gen(path):
-        #import time, random
-        #time.sleep(random.random())
-        return "Gutta cavat capidem\n" * 500
+        import random
+        return "Gutta cavat capidem\n" * random.randint(100,500)
 
 def fun_map(e, params):
         return [(w, 1) for w in re.sub("\W", " ", e).lower().split()]
@@ -21,7 +20,7 @@ def fun_reduce(iter, out, params):
 
 tserver.run_server(data_gen)
 name = disco.job(sys.argv[1], "test_50k", tserver.makeurl([""] * int(5e4)),
-                       fun_map, reduce = fun_reduce, nr_reduces = 320)
+                       fun_map, reduce = fun_reduce, nr_reduces = 300)
 
 for key, value in disco.result_iterator(results):
 	print key, value
