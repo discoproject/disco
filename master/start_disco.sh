ba87f9a6dae517f9deda3f5f14f29d4052f19199
Ville Tuulos <tuulos@nxfront.nokiapaloalto.com>
2008-06-04 18:58:52 -0700

    added PREFIX and DISCO_PORT parameters to start_disco

--- master/start_disco.sh
+++ master/start_disco.sh
@@ -3,4 +3,16 @@ then
         echo "[]" >> disco.cfg
 fi
 
-PATH=.:$PATH erl +K true -smp on -pa ebin -pa src -boot disco -disco scgi_port 4444 -disco disco_config disco.cfg -kernel error_logger '{file, "disco.log"}' -eval "[handle_job, handle_ctrl]"
+if [ -z $DISCO_PORT ]
+then
+        DISCO_PORT=4444
+fi
+
+if [ -z $PREFIX ]
+then
+	PREFIX="disco"
+fi
+
+export SLAVENAME=$PREFIX"_slave"
+
+PATH=.:$PATH erl +K true -sname $PREFIX"_master" -rsh ssh -smp on -pa ebin -pa src -boot disco -disco scgi_port $DISCO_PORT -disco disco_config disco.cfg -kernel error_logger '{file, "disco.log"}' -eval "[handle_job, handle_ctrl]"
