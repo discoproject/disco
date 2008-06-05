037b883a4e84c033b75e5060fdb816fcd39d1110
Ville Tuulos <tuulos@nxfront.nokiapaloalto.com>
2008-06-05 02:26:46 -0700

    fixed a bug in test_external.py

--- test/test_external.py
+++ test/test_external.py
@@ -10,7 +10,7 @@ def fun_reduce(iter, out, params):
         
 tserver.run_server(data_gen)
 
-inputs = ["dog", "cat", "ape"]
+inputs = ["ape", "cat", "dog"]
 params = {"test1": "1,2,3",\
           "one two three": "dim\ndam\n",\
           "dummy": "value"}
@@ -22,9 +22,10 @@ results = disco.job(sys.argv[1], "test_external", tserver.makeurl(inputs),
 		nr_reduces = 1,
 		sort = False)
 
-results = list(disco.result_iterator(results))
-for k, v in results: 
-        if k != "red_dkey" and v != "red_test_dog":
+results = sorted([(v, k) for k, v in disco.result_iterator(results)])
+for i, e in enumerate(results): 
+	v, k = e
+        if k != "red_dkey" or v != "red_test_%s" % inputs[i / 3]:
                 raise Exception("Invalid answer: %s, %s" % (k, v))
 
 if len(results) != 9:
