6444b10cf46fd57802b92c1cc190a7e24eec2af4
Ville Tuulos <tuulos@nxfront.nokiapaloalto.com>
2008-07-05 01:37:59 -0700

    added NATIVE flag for the compile script to choose between hipe and non-hipe compilation

--- master/compile.sh
+++ master/compile.sh
@@ -1,2 +1,8 @@
-erlc +native +"{hipe, [o3]}" -o ebin src/*.erl
+if [ $NATIVE ]; then
+	echo "Compile with Hipe"
+	erlc +native +"{hipe, [o3]}" -o ebin src/*.erl
+else
+	echo "No Hipe"
+	erlc -o ebin src/*.erl
+fi
 erl -pa ebin -noshell -run make_boot write_scripts
