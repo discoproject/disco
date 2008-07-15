00f12f5fd7e9bce472a75f4a8f707e518c5b1798
Ville Tuulos <tuulos@nxfront.nokiapaloalto.com>
2008-07-15 02:55:33 -0700

    added kill -option to discoadmin

--- util/discoadmin.py
+++ util/discoadmin.py
@@ -6,15 +6,19 @@ OK_STATUS = ['job_ready', 'job_died']
 
 disco = Disco(sys.argv[1])
 
-def op_show(n):
+def op_show(n, s):
         print n
 
-def op_clean(n):
-        print "Cleaning", n
-        disco.clean(n)
+def op_kill(n, s):
+        if s == "job_active":
+                print "Killing", n
+                disco.kill(n)
+
+def op_clean(n, s):
+        if s in OK_STATUS:
+                print "Cleaning", n
+                disco.clean(n)
 
 for t, s, name in disco.joblist():
-        if s not in OK_STATUS:
-                continue
         if sys.argv[3] in name:
-                globals()["op_" + sys.argv[2]](name)
+                globals()["op_" + sys.argv[2]](name, s)
