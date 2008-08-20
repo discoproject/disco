8f8e9a7d38824202c47b8609691e2711b8a28dae
Ville Tuulos <tuulos@nxfront.nokiapaloalto.com>
2008-08-19 17:47:21 -0700

    Renamed disco_worker to disco-worker for symmetry (cf. disco-master)

--- node/setup.py
+++ node/setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 
 setup(name = "disconode",
       version = "0.1",
-      scripts = ["disco_worker"],
+      scripts = ["disco-worker"],
       packages = find_packages())
 
