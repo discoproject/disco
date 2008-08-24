6f4aeec1c8e404da3102f89f2c9b1c5d34845bd2
Ville Tuulos <tuulos@nxfront.nokiapaloalto.com>
2008-08-24 03:36:18 -0700

    distrfiles is now AWS-compatible

--- util/distrfiles.py
+++ util/distrfiles.py
@@ -1,13 +1,43 @@
 import os, sys, math, random, os
 from subprocess import *
- 
+
+if len(sys.argv) < 3:
+        print >> sys.stderr, "\nUsage: distrfiles.py <dataset-path> <nodes-file>\n"
+        print >> sys.stderr, "DISCO_ROOT must specify the disco home directory (e.g. /srv/disco/)."
+        print >> sys.stderr, "SSH_KEY can specify a value for the -i flag of ssh / scp."
+        print >> sys.stderr, "SSH_USER can specify the user name."
+        print >> sys.stderr, "REMOVE_FIRST if set, removes previous dataset of the "\
+                             "same name before copying.\n"
+        sys.exit(1)
+
+if "DISCO_ROOT" not in os.environ:
+        print >> sys.stderr, "Specify DISCO_ROOT "\
+                "(data lives at $DISCO_ROOT/data)"
+        sys.exit(1)
+
+root = os.environ["DISCO_ROOT"]
+ssh = []
+if "SSH_KEY" in os.environ:
+        ssh += ["-i", os.environ["SSH_KEY"]]
+
+user = os.environ.get("SSH_USER", "")
+
 files = os.listdir(sys.argv[1])
 if sys.argv[1][-1] == "/":
 	name = os.path.basename(sys.argv[1][:-1])
 else:
 	name = os.path.basename(sys.argv[1])
 
-nodes = file(sys.argv[2]).readlines()
+nodes = []
+hostname = {}
+for l in file(sys.argv[2]):
+        s = l.strip().split()
+        if len(s) > 1:
+                hostname[s[0]] = s[1]
+        else:
+                hostname[s[0]] = s[0]
+        nodes.append(s[0])
+
 fno = int(math.ceil(max(1, float(len(files)) / len(nodes))))
 
 print >> sys.stderr, "Dataset name: %s" % name
@@ -23,17 +53,20 @@ while True:
 	if not nset:
 		break
 	files = files[fno:]
-	node = nodes.next().strip()
-	
-	if "REMOVE_FIRST" in os.environ:
-		call(["ssh", node, "rm -Rf /var/disco/" + name])
+	node = unode = nodes.next().strip()
+	if user:
+                unode = "%s@%s" % (user, node)
 
-	if call(["ssh", node, "mkdir /var/disco/%s" % name]):
-		print >> sys.stderr, "Couldn't mkdir %s:/var/disco/%s" % (node, name)
+	if "REMOVE_FIRST" in os.environ:
+		call(["ssh"] + ssh + [unode, "rm -Rf %s/data/%s" % (root, name)])
+
+	if call(["ssh"] + ssh + [unode, "mkdir /%s/data/%s" % (root, name)]):
+		print >> sys.stderr, "Couldn't mkdir %s:%s/data/%s" % (node, root, name)
 
 	print >> sys.stderr, "Copying %d files to %s" % (len(nset), node)
-	p = Popen("nice -19 ionice -n 7 scp " + " ".join(nset) +
-                  " %s:/var/disco/%s/" % (node, name), shell = True)
+	p = Popen("nice -19 ionice -n 7 scp %s %s %s:%s/data/%s/" % 
+                (" ".join(ssh), " ".join(nset), unode, root, name),
+                        shell = True)
 	procs.append((node, p, nset))
 
 print >> sys.stderr, "Waiting for copyers to finish.."
@@ -44,7 +77,8 @@ for node, p, nset in procs:
 	else:
 		print >> sys.stderr, "%s ok" % node
 		print "\n".join(("disco://%s/%s/%s" %
-                        (node, name, os.path.basename(f)) for f in nset))
+                        (hostname[node], name, os.path.basename(f))
+                                for f in nset))
 
 print >> sys.stderr, "Done"
 
