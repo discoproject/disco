01bed8ce93142bdc0fb3a9c20f09fd49b8f76142
Ville Tuulos <tuulos@nxfront.nokiapaloalto.com>
2009-04-04 22:36:40 -0700

    Removed unnecessary ANS in test_chunked

--- test/test_chunked.py
+++ test/test_chunked.py
@@ -2,9 +2,6 @@
 import tserver, sys
 from disco import Disco, result_iterator
 
-ANS = "1028380578493512611198383005758052057919386757620401"\
-      "58350002406688858214958513887550465113168573010369619140625"
-
 def data_gen(path):
         return path[1:] + "\n"
 
