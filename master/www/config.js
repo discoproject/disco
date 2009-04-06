d100ac1e2117789e5061ade04153aa5d41db3d80
Ville Tuulos <tuulos@nxfront.nokiapaloalto.com>
2009-04-06 02:30:17 -0700

    Revert "Fix for sending invalid configuration data. [#1 state:resolved]"
    This reverts commit e36a7041c503a9be5b7d2768bd64c46a6819203f.

--- master/www/config.js
+++ master/www/config.js
@@ -66,7 +66,7 @@ function add_to_blacklist(){
 function send_table(){
         var table = $("tbody > tr").map(function(){
                 var arr = $.makeArray($(this).children().map(function(){
-                        return jQuery.trim($(this).text() || $(this).find('input').val());}));
+                          return jQuery.trim($(this).text());}));
                 arr.shift();  // throw away the first node - 'remove'
                 return JSON.stringify(arr);
         });
