bb6d5de0098478f2d44fc8b74a4db1c7dc530a18
Ville Tuulos <tuulos@nxfront.nokiapaloalto.com>
2009-05-13 20:44:35 -0700

    Improved test execution script

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
 export PYTHONPATH=../pydisco
+TESTS=(test_*.py)
+NUM_TESTS=${#TESTS[*]}
 
 if [[ -z $1 ]]
 then
-        echo "Usage:"
-        echo "./run_tests.sh disco://master [test_name]"
+        echo -e "\nUsage:"
+        echo -e "\n./run_tests.sh disco://master [test id]"
+        echo -e "\nTests:\n"
+        for ((i=0; i<$NUM_TESTS; i++));
+        do
+                echo "$i) ${TESTS[$i]}"
+        done
+        echo
         exit
 fi
 
-for tst in test_*.py
+if [[ $2 ]]
+then
+	FIRST=$2
+else
+	FIRST=0
+fi
+
+for ((i=$FIRST; i<$NUM_TESTS; i++));
 do
-        if [[ ! -z $2 && $tst != $2 ]]
-        then
-                continue
-        fi
-        echo "Running $tst"
-        python $tst $1 2> $tst.err.log > $tst.log
+	tst=${TESTS[$i]}
+        echo "$i) $tst"
+        time python $tst $1 2> $tst.err.log > $tst.log
         if [[ `tail -1 $tst.log` == "ok" ]]
         then
                 echo "Test $tst ok."
         else
                 echo "Test $tst failed. See $tst.*.log"
-                break
         fi
+	echo
 done
