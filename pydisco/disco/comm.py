c1ddcb76e4b8a5192a9573053ada03b004154c44
Ville Tuulos <tuulos@nxfront.nokiapaloalto.com>
2009-07-25 15:17:57 -0700

    bugfix: pycurl wasn't checked correctly in comm.py - client failed if curl was not installed

--- pydisco/disco/comm.py
+++ pydisco/disco/comm.py
@@ -1,11 +1,11 @@
 import os
 
+nocurl = "nocurl" in os.environ.get("DISCO_FLAGS", "").lower().split()
+
 try:
         import pycurl
 except:
         nocurl = True
-
-nocurl = "nocurl" in os.environ.get("DISCO_FLAGS", "").lower().split()
 
 if nocurl:
         from comm_httplib import *
