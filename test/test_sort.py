9c3821853c3652a0995dae0075ccd965312e715e
Ville Tuulos <tuulos@dxfront.(none)>
2009-08-23 05:25:49 +0000

    rewrote test_sort.py to detect the base64-bug with external sort and another bug related to the -s and -n flags in sort.

--- test/test_sort.py
+++ test/test_sort.py
@@ -1,40 +1,62 @@
 
-import tserver, sys, disco
+import tserver, sys, string, base64
+from disco import Disco, result_iterator
 
 def data_gen(path):
         return "\n"
 
 def fun_map(e, params):
-        import random
-        return [(random.randint(0, 2**31), 0) for x in range(100)]
+        r = []
+        for x in string.ascii_lowercase:
+                r += list(x * 10)
+        for x in range(10):
+                r += [str(i) for i in range(10)]
+        random.shuffle(r)
+        # test base64 encoding at the same time
+        # no other reason for using base64 here
+        return [(base64.encodestring(x), '') for x in r]
 
 def fun_reduce(iter, out, params):
-        prev = 0
-        for k, v in iter:
-                k = int(k)
-                if k < prev:
-                        raise "Val %s < %s -> sorting failed" % (k, prev)
-                prev = k
+        d = {}
+        for k, v in itertools.groupby(iter, key = lambda x: x[0]):
+                if k in d:
+                        raise Exception("Key %s seen already. "\
+                                "Sorting failed." % k)
+                d[k] = len(list(v))
+        for k, v in d.iteritems():
                 out.add(k, v)
 
-def run_disco(limit):
-        results = disco.job(sys.argv[1], "test_sort",
-                                tserver.makeurl([""] * int(1e3)),
-                                fun_map, reduce = fun_reduce, nr_reduces = 50,
-                                sort = True, mem_sort_limit = limit)
-        k = len(list(disco.result_iterator(results)))
-        if k != int(1e5): 
-                raise "not enough results: Got %d, expected %d" % (k, 1e5)
-        disco.Disco(sys.argv[1]).purge(disco.util.jobname(results[0]))
+def run_disco(limit, name):
+        job = disco.new_job(
+                        name = "test_sort_%s" % name,
+                        input = tserver.makeurl([""] * int(100)),
+                        map = fun_map,
+                        reduce = fun_reduce,
+                        nr_reduces = 1,
+                        sort = True,
+                        mem_sort_limit = limit)
 
+        ANS = dict((str(x), True)\
+                for x in list(string.ascii_lowercase) + range(10))
+
+        for k, v in result_iterator(job.wait()):
+                if v != "1000":
+                        raise Exception("Incorrect result: "\
+                                "Expected 1000, got %s" % v)
+                del ANS[base64.decodestring(k)]
+        if ANS:
+                raise Exception("Missing keys: %s" % " ".join(ANS.keys()))
+        job.purge()
+
+disco = Disco(sys.argv[1])
 tserver.run_server(data_gen)
 
 print "Testing in-memory sort"
-run_disco(1024**4)
+run_disco(1024**4, "inmemory")
 print "in-memory ok"
 
 print "Testing external sort"
-run_disco(0)
+run_disco(0, "external")
 print "external ok"
 print "ok"
 
