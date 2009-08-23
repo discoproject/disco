discoproject/disco

commit dd3b435ebd45c153924a08432ca89e118f39d29d

Replaced the erlang application .boot script with the simple -eval 'application:start(disco)'. It appears to be practically impossible to make .boot scripts compatible across different versions of erlang.

Makefile
@@ -20,12 +20,10 @@ TARGETCFG = $(DESTDIR)/$(CONFIG_DIR)
 SRC = $(wildcard $(ESRC)/*.erl)
 TARGET = $(addsuffix .beam, $(basename \
              $(addprefix $(EBIN)/, $(notdir $(SRC)))))
-BOOT = master/disco.boot
-APP = master/ebin/disco.app
 
 build: master
 
-master: $(TARGET) $(APP)
+master: $(TARGET)
 
 clean:
 	- rm $(TARGET) $(BOOT)
@@ -68,11 +66,6 @@ install-config:
 	$(if $(wildcard $(TARGETCFG)/disco.conf),\
 		$(info disco config already exists, skipping),\
 		install -m 0644 conf/disco.conf.example $(TARGETCFG)/disco.conf)
-
-$(APP): $(BOOT)
-
-$(BOOT):
-	(cd master; erl -pa ebin -noshell -run make_boot write_scripts)
 
 $(EBIN)/%.beam: $(ESRC)/%.erl
 	$(CC) $(OPT) -o $(EBIN) $<

master/disco-master
@@ -63,16 +63,16 @@ erl +K true\
     -rsh ssh\
     -connect_all false\
     -pa "$DISCO_HOME/ebin"\
-    -boot "$DISCO_HOME/disco"\
     -kernel error_logger "{file, \"$DISCO_MASTER_LOG\"}"\
-    -eval "[handle_job, handle_ctrl]"\
     -disco disco_name \"$DISCO_NAME\"\
     -disco disco_root \"$DISCO_MASTER\"\
     -disco disco_localdir \"$DISCO_LOCALDIR\"\
     -disco disco_master_host \"$DISCO_MASTER_HOST\"\
     -disco disco_slaves_os \"$DISCO_SLAVES_OS\"\
     -disco scgi_port $DISCO_SCGI_PORT\
-    -disco disco_config \"$DISCO_CONFIG\" </dev/null >/dev/null 2>/dev/null
+    -disco disco_config \"$DISCO_CONFIG\"\
+    -eval "[handle_job, handle_ctrl]"\
+    -eval "application:start(disco)" </dev/null >/dev/null 2>/dev/null
 
 sleep 1
 

master/src/disco_main.erl
@@ -22,8 +22,7 @@ write_pid(PidFile) ->
                         ["Could not write PID to ", PidFile, ":", Error])
         end.
 
-resultfs_enabled() -> true.
-resultfs_enabled0() ->
+resultfs_enabled() ->
         case os:getenv("DISCO_FLAGS") of
                 false -> false;
                 Flags ->

master/src/make_boot.erl
@@ -1,21 +1,0 @@
--module(make_boot).
--export([write_scripts/0]).
-
-write_scripts() -> 
-        Erts = erlang:system_info(version),
-        {value, {kernel, _, Kernel}} = lists:keysearch(kernel, 1,
-                application:loaded_applications()),
-        {value, {stdlib, _, Stdlib}} = lists:keysearch(stdlib, 1,
-                application:loaded_applications()),
-
-        Rel = "{release, {\"Disco\", \"1\"}, {erts, \"~s\"}, ["
-               "{kernel, \"~s\"}, {stdlib, \"~s\"}, {disco, \"1\"}]}.",
-
-        {ok, Fs} = file:open("disco.rel", [write]),
-        io:format(Fs, Rel, [Erts, Kernel, Stdlib]),
-        file:close(Fs),
-        
-        systools:make_script("disco", [local]),
-        halt().
-
-

commit bb56645b8b067d38dcd8d0f69afff721b9cc8eae

Fixed Makefile which was broken by the commit dd3b435ebd

Makefile
@@ -26,7 +26,6 @@ build: master
 master: $(TARGET)
 
 clean:
-	- rm $(TARGET) $(BOOT)
 	- rm -Rf master/disco.rel master/disco.script 
 	- rm -Rf pydisco/build
 	- rm -Rf pydisco/disco.egg-info
@@ -38,8 +37,7 @@ install: install-master install-pydisco install-node
 install-master: install-config master
 	install -d $(TARGETDIR)/ebin
 	install -d $(TARGETBIN)
-	install -m 0755 $(BOOT) $(TARGETDIR)
-	install -m 0755 $(APP) $(TARGETDIR)/ebin
+	install -m 0755 master/ebin/disco.app $(TARGETDIR)/ebin
 	install -m 0755 master/make-lighttpd-proxyconf.py $(TARGETDIR)
 	install -m 0755 master/disco-master $(TARGETBIN)
 