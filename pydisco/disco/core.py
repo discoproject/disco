discoproject/disco

commit 46ccd2ce5d843d14c07074fefc43f38477a4683b

refactored the eventmonitor.py for readability/extensibility

pydisco/disco/core.py
@@ -44,12 +44,14 @@ class Stats(object):
                 pass
 
 class Disco(object):
-
         def __init__(self, host):
                 self.host = "http://" + util.disco_host(host)[7:]
 
         def request(self, url, data = None, redir = False, offset = 0):
-                return comm.download(self.host + url, data = data, redir = redir, offset = offset)
+                try:
+                        return comm.download(self.host + url, data = data, redir = redir, offset = offset)
+                except Exception, e:
+                        raise DiscoError('Got %s, make sure disco master is running at %s' % (e, self.host))
 
         def nodeinfo(self):
                 return json.loads(self.request("/disco/ctrl/nodeinfo"))
@@ -103,8 +105,8 @@ class Disco(object):
                 self.request("/disco/ctrl/purge_job", '"%s"' % name)
 
         def jobspec(self, name):
-                r = self.request("/disco/ctrl/parameters?name=%s"\
-                        % name, redir = True)
+                r = self.request("/disco/ctrl/parameters?name=%s" % name,
+                                 redir = True)
                 return decode_netstring_fd(cStringIO.StringIO(r))
 
         def events(self, name, offset = 0):
@@ -126,82 +128,66 @@ class Disco(object):
                                         offs -= 1
                                 yield offs, ent
                 
-                r = self.request("/disco/ctrl/rawevents?name=%s"\
-                         % name, redir = True, offset = offset)
+                r = self.request("/disco/ctrl/rawevents?name=%s" % name,
+                                 redir = True, offset = offset)
                 
                 if len(r) < 2:
                         return []
                 else:
                         return event_iter(r)
 
-        def results(self, names, timeout = 2000):
-                single = type(names) == str
-                if single:
-                        names = [names]
+        def results(self, jobspec, timeout = 2000):
+                jobspec = JobSpecifier(jobspec)
+                data    = json.dumps([timeout, list(jobspec.jobnames)])
+                results = json.loads(self.request("/disco/ctrl/get_results", data))
+
+                if len(jobspec) is 1:
+                        return results[0][1]
                 
-                nam = []
-                for n in names:
-                        if type(n) == str:
-                                nam.append(n)
-                        elif type(n) == list:
-                                nam.append(n[0])
+                others, active = [], []
+                for result in results:
+                        if result[1][0] == 'active':
+                                active.append(result)
                         else:
-                                nam.append(n.name)
-               
-                r = self.request("/disco/ctrl/get_results",
-                        json.dumps([timeout, nam]))
-                if not r:
-                        return None
-                r = json.loads(r)
-                if single:
-                        return r[0][1]
-                else:
-                        active = []
-                        others = []
-                        for x in r:
-                                if x[1][0] == "active":
-                                        active.append(x)
-                                else:
-                                        others.append(x)
-                        return others, active
+                                others.append(result)
+                return others, active
 
         def jobinfo(self, name):
-                r = self.request("/disco/ctrl/jobinfo?name=" + name)
-                if r:
-                        return json.loads(r)
-                else:
-                        return r
+                jobinfo = self.request("/disco/ctrl/jobinfo?name=%s" % name)
+                if jobinfo:
+                        return json.loads(jobinfo)
 
-        def wait(self, name, show_events = None, poll_interval = 5,\
-                        timeout = None, clean = False):
-                
-                mon = EventMonitor(show_events,\
-                        disco = self, name = name)
-                t = time.time()
-                if mon.isenabled():
-                        p = 2000
-                else:
-                        p = poll_interval * 1000
+        def wait(self, name, show = None, poll_interval = 2, timeout = None, clean = False):
+                event_monitor = EventMonitor(show, disco=self, name=name)
+                start_time    = time.time()
                 while True:
-                        mon.refresh()
-                        status = self.results(name, timeout = p)
-                        if status == None:
-                                raise JobException("Unknown job", self.host, name)
-                        if status[0] == "ready":
-                                if mon.isenabled():
-                                        time.sleep(p / 1000.0 + 1)
-                                        mon.refresh()
+                        status, results = self.results(name, timeout = poll_interval * 1000)
+                        event_monitor.refresh()
+                        if status == 'ready':
                                 if clean:
                                         self.clean(name)
-                                return status[1]
-                        if status[0] != "active":
-                                if mon.isenabled():
-                                        time.sleep(p / 1000.0 + 1)
-                                        mon.refresh()
-                                raise JobException("Job failed", self.host, name)
-                        if timeout and time.time() - t > timeout:
+                                return results
+                        if status != 'active':
+                                raise JobException("Job status %s" % status, self.host, name)
+                        if timeout and time.time() - start_time > timeout:
                                 raise JobException("Timeout", self.host, name)
 
+class JobSpecifier(list):
+        def __init__(self, jobspec):
+                super(JobSpecifier, self).__init__([jobspec] if type(jobspec) is str else jobspec)
+
+        @property
+        def jobnames(self):
+                for job in self:
+                        if type(job) is str:
+                                yield job
+                        elif type(job) is list:
+                                yield job[0]
+                        else:
+                                yield job.name
+
+        def __str__(self):
+                return '{%s}' % ', '.join(self.jobnames)
 
 class Job(object):
 
@@ -256,7 +242,7 @@ class Job(object):
         def _run(self, **kw):
                 d = lambda x: kw.get(x, Job.defaults[x])
                 
-                # -- check parametets --
+                # -- check parameters --
 
                 # Backwards compatibility 
                 # (fun_map == map, input_files == input)

pydisco/disco/error.py
@@ -2,10 +2,10 @@ class DiscoError(Exception):
     pass
 
 class JobException(DiscoError):
-    def __init__(self, msg, master=None, name=None):
-        self.msg = msg
-        self.name = name
-        self.master = master
+    def __init__(self, error, master=None, jobname=None):
+        self.error   = error
+        self.master  = master
+        self.jobname = jobname
         
     def __str__(self):
-        return "Job %s/%s failed: %s" % (self.master, self.name, self.msg)
+        return "Job %s/%s failed: %s" % (self.master, self.jobname, self.error)

pydisco/disco/eventmonitor.py
@@ -1,134 +1,175 @@
-import sys
-from os import environ
+import sys, os, time
 from disco.comm import json
 from disco.error import DiscoError
 
-# Following from Python cookbook, #475186
-def has_colors(stream):
-        global curses
-        if not hasattr(stream, "isatty"):
-                return False
-        if not stream.isatty():
-                return False # auto color only on TTYs
-        try:
-                import curses
-                curses.setupterm()
-                return curses.tigetnum("colors") > 2
-        except:
-                # guess false in case of error
-                return False
+try:
+        import curses
+except ImportError:
+        curses = None
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+class OutputStream(object):
+        def __init__(self, format, handle=sys.stdout):
+                self.handle    = handle
+                self.isenabled = True
+
+                if not format:
+                        self.isenabled = False
+                        self.writer    = EventWriter(handle)
+                elif format == 'json':
+                        self.writer    = JSONEventWriter(handle)
+                elif format == 'nocolor' or not self.hascolor:
+                        self.writer    = TextEventWriter(handle)
+                else:
+                        self.writer    = ANSIEventWriter(handle)
+
+        @property
+        def hascolor(self): # Based on Python cookbook, #475186
+                try:
+                        if self.handle.isatty():
+                                curses.setupterm()
+                                return curses.tigetnum('colors') > 2
+                except:
+                        pass
+
+        def write(self, *args, **kwargs):
+                self.writer.write(*args, **kwargs)
+
+class EventWriter(object):
+        def __init__(self, handle):
+                self.handle = handle
+
+        def write(self, *args, **kwargs):
+                pass
+
+class TextEventWriter(EventWriter):
+        def write(self, status=None, timestamp=None, host=None, message=None):
+                if timestamp:
+                        self.handle.write('%s %s %s\n' % (timestamp, host, message))
+                elif status:
+                        self.handle.write('%s\n' % status)
+
+class ANSIEventWriter(EventWriter):
+        def __init__(self, handle):
+                super(ANSIEventWriter, self).__init__(handle)
+
+        @staticmethod
+        def background(color):
+                return curses.tparm(curses.tigetstr('setab'), color)
+
+        @staticmethod
+        def foreground(color):
+                return curses.tparm(curses.tigetstr('setaf'), color)
+
+        @property
+        def reset(self):
+                return curses.tigetstr('sgr0')
+
+        @property
+        def end_line(self):
+                return self.reset + curses.tigetstr("el")
+
+        def ansi_text(self, text, bgcolor=WHITE, fgcolor=BLACK):
+                return self.background(bgcolor) + self.foreground(fgcolor) + text
+
+        def colorbar(self, length, color=WHITE):
+                return self.ansi_text(' ' * length, bgcolor=color)
+
+        def format(self, text):
+                if text.startswith('ERROR'):
+                        return self.error(text)
+                elif text.startswith('WARN'):
+                        return self.warning(text)
+                elif text.startswith('READY'):
+                        return self.ready('%s ' % text)
+                return self.message(text)
+
+        def error(self, error):
+                return self.ansi_text(error, fgcolor=RED) + self.end_line
+
+        def host(self, host):
+                return self.ansi_text(host, fgcolor=BLUE)
+
+        def message(self, message):
+                return self.ansi_text(message) + self.end_line
+
+        def heading(self, heading):
+                return self.ansi_text(heading, bgcolor=BLUE, fgcolor=WHITE) + self.end_line
+
+        def ready(self, ready):
+                 return self.ansi_text(' %s' % ready, bgcolor=GREEN, fgcolor=WHITE) + self.end_line
+
+        def status(self, status):
+                return self.ansi_text(status, fgcolor=CYAN) + self.end_line
+
+        def timestamp(self, timestamp):
+                return self.ansi_text(timestamp, fgcolor=GREEN)
+
+        def warning(self, warning):
+                return self.ansi_text(warning, fgcolor=MAGENTA) + self.end_line
+
+        def write(self, status=None, timestamp=None, host=None, message=None):
+                if status:
+                        return self.handle.write('%s\n' % self.status(status))
+
+                if not timestamp:
+                        return self.handle.write('%s\n' % self.heading(message + ":"))
+
+                self.handle.write('%s %s %s\n' % (self.timestamp('%s ' % timestamp),
+                                                  self.host('%-10s' % host),
+                                                  self.format(message)))
+
+class JSONEventWriter(EventWriter):
+        def write(self, status=None, timestamp=None, host=None, message=None):
+                if timestamp:
+                        print json.dumps([timestamp, host, message])
 
 
 class EventMonitor(object):
         def __init__(self, show, disco = None, name = None, job = None):
                 if job:
-                        self.disco = job.master
-                        self.name = job.name
+                        self.disco, self.name = job.master, job.name
                 else:
-                        self.disco = disco
-                        self.name = name
+                        self.disco, self.name = disco, name
 
-                if not disco:
+                if not self.disco:
                         raise DiscoError("Specify either job or disco and name")
 
-                self.offset = 0
-                self.output = None
+                self.offset      = 0
                 self.prev_status = None
+                self.output      = OutputStream(format=(show or os.getenv('DISCO_EVENTS')))
+                self.output.write(message=self.name)
 
-                if show == None:
-                        show = environ.get("DISCO_EVENTS", None)
-                if show:
-                        if type(show) == str:
-                                show = show.lower()
-                        if show == "json":
-                                self.output = self.json_output
-                        elif not has_colors(sys.stdout) or show == "nocolor":
-                                self.output = self.plain_output
-                        else:
-                                self.init_ansi()
-                                self.output = self.ansi_output
-                if self.output:
-                        self.output(None, None, None, self.name)
+        @property
+        def events(self):
+                return self.disco.events(self.name, self.offset)
 
-        def init_ansi(self):
-                fg = curses.tigetstr("setaf")
-                bg = curses.tigetstr("setab")
-                self.reset = curses.tigetstr("sgr0")
-                el = curses.tigetstr("el")
-                new_bg = curses.tparm(bg, 4)
-                msg_bg = curses.tparm(bg, 7)
-                done_bg = curses.tparm(bg, 2)
-                self.new = lambda x:\
-                        new_bg + curses.tparm(fg, 7) + x + msg_bg + el 
-                self.status = lambda x: msg_bg + curses.tparm(fg, 6) + x + el
-                self.tstamp = lambda x: msg_bg + curses.tparm(fg, 2) + x
-                self.host = lambda x: msg_bg + curses.tparm(fg, 4) + x 
-                self.msg = lambda x: msg_bg + curses.tparm(fg, 0) + x + el
-                self.error = lambda x: msg_bg + curses.tparm(fg, 1) + x + el
-                self.warn = lambda x: msg_bg + curses.tparm(fg, 5) + x + el
-                self.done = lambda x: "%s %s%s%s%s%s" %\
-                        (msg_bg, done_bg, curses.tparm(fg, 7), x, msg_bg, el)
-
+        @property
         def isenabled(self):
-                return self.output != None
+                return self.output.isenabled
+
+        @property
+        def stats(self):
+                jobinfo = self.disco.jobinfo(self.name)
+                if sum(jobinfo['redi'][1:]):
+                        return ['reduce'] + jobinfo['redi']
+                return ['map'] + jobinfo['mapi']
+
+        @property
+        def status(self):
+                return "Status: [%s] %d waiting, %d running, %d done, %d failed" % tuple(self.stats)
+
+        def log_events(self):
+                for offset, (timestamp, host, message) in self.events:
+                        self.offset = offset
+                        self.output.write(timestamp=timestamp, host=host, message=message)
 
         def refresh(self):
-                if not self.output:
-                        return
-                info = self.disco.jobinfo(self.name)
-                if sum(info['redi'][1:]):
-                        n = tuple(["reduce"] + info['redi'])
-                else:
-                        n = tuple(["map"] + info['mapi'])
-                
-                status = "Status: [%s] %d waiting, %d running, "\
-                         "%d done, %d failed" % n
-
-                i = 0
-                for offs, event in self.disco.events(self.name, self.offset):
-                        self.offset = offs
-                        tstamp, host, msg = event
-                        self.output(None, tstamp, host, msg)
-                        i += 1
-                if i and self.prev_status != status:
-                        self.output(status, None, None, None)
-                        self.prev_status = status
-
-
-        def plain_output(self, status, tstamp, host, msg):
-                if tstamp:
-                        print tstamp, host, msg
-                elif status:
-                        print status
-
-        def json_output(self, status, tstamp, host, msg):
-                if tstamp:
-                        print json.dumps([tstamp, host, msg])
-        
-        def ansi_output(self, status, tstamp, host, msg):
-                if status:
-                        print self.status(status) + self.reset
-                        return
-                elif not tstamp:
-                        print self.new(msg + ":") + self.reset
-                        return
-
-                msg = " " + msg
-                if msg.startswith(" ERROR"):
-                        m = self.error
-                elif msg.startswith(" WARN"):
-                        m = self.warn
-                elif msg.startswith(" READY"):
-                        m = self.done
-                        msg = msg + " "
-                else:
-                        m = self.msg
-                txt = "\n".join(m(x) for x in (msg).splitlines())
-                print "%s%s%s%s" % (self.tstamp(tstamp + " "),\
-                        self.host("%-10s" % host), txt, self.reset)
-
-
-
-
-
-
+                if self.isenabled:
+                        status = self.status
+                        self.log_events()
+                        if self.prev_status != status:
+                                self.output.write(status=status)
+                                self.prev_status = status
+                        time.sleep(2)

commit 9611f000e7b8b796c7bef26170946ee8d1f38e7e

bugfix: fixed a regression introduced by 46ccd2ce in disco.core.results() - results were not correct if jobspec was a list of only one element

pydisco/disco/core.py
@@ -141,7 +141,7 @@ class Disco(object):
                 data    = json.dumps([timeout, list(jobspec.jobnames)])
                 results = json.loads(self.request("/disco/ctrl/get_results", data))
 
-                if len(jobspec) is 1:
+                if type(jobspec) == str:
                         return results[0][1]
                 
                 others, active = [], []
@@ -174,7 +174,8 @@ class Disco(object):
 
 class JobSpecifier(list):
         def __init__(self, jobspec):
-                super(JobSpecifier, self).__init__([jobspec] if type(jobspec) is str else jobspec)
+                super(JobSpecifier, self).__init__([jobspec]
+                        if type(jobspec) is str else jobspec)
 
         @property
         def jobnames(self):