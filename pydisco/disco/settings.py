828421786d1574087fe5709d35d5c25e2b350b76
Jared Flatow <jflatow@gmail.com>
2009-10-05 19:19:24 -0700

    Revert "Don't set DISCO_DEBUG by default"
    This reverts commit dc4e3071f0791810f6d11d666c3b2405422266ae.

--- pydisco/disco/settings.py
+++ pydisco/disco/settings.py
@@ -2,6 +2,7 @@ import os, socket
 
 class DiscoSettings(dict):
     defaults = {
+        'DISCO_DEBUG':           "1",
         'DISCO_EVENTS':          "''",
         'DISCO_FLAGS':           "''",
         'DISCO_LOG_DIR':         "'/var/log/disco'",
