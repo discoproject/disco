0824118c15749031e6fb85b39e478ad94266e913
Jared Flatow <jflatow@gmail.com>
2010-03-11 06:44:16 -0800

    switch nchunksbalance to be uniform random distribution, move round robin to roundrobinbalance

--- lib/discodex/mapreduce/balancers/__init__.py
+++ lib/discodex/mapreduce/balancers/__init__.py
@@ -1,3 +1,7 @@
 def nchunksbalance(key, nr_reduces, params):
+    from random import randint
+    return randint(0, nr_reduces - 1)
+
+def roundrobinbalance(key, nr_reduces, params):
     params.n += 1
     return params.n % nr_reduces
