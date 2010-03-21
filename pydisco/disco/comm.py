35a25131cc30c172e6673575b359d1d3efdf120f
Ville Tuulos <ville.h.tuulos@nokia.com>
2010-03-21 06:38:40 +0000

    chunked data transfer implemented - data is now moved in 10M chunks instead of persistent http connections

--- pydisco/disco/comm.py
+++ pydisco/disco/comm.py
@@ -1,8 +1,10 @@
-import os
+import os, time, cStringIO, struct
+from disco.error import CommError
 
 import disco.settings
 
 BUFFER_SIZE = int(1024**2)
+CHUNK_SIZE = int(10 * 1024**2)
 
 settings = disco.settings.DiscoSettings()
 nocurl = "nocurl" in settings["DISCO_FLAGS"].lower().split()
@@ -13,16 +15,9 @@ except ImportError:
     nocurl = True
 
 if nocurl:
-    from disco.comm_httplib import *
+    from disco.comm_httplib import upload, download as real_download
 else:
-    from disco.comm_curl import *
-
-def open_local(path, url):
-    # XXX: wouldn't it be polite to give a specific error message if this
-    # operation fails
-    fd = file(path, "r", BUFFER_SIZE)
-    sze = os.stat(path).st_size
-    return (fd, sze, "file://" + path)
+    from disco.comm_curl import upload, download as real_download
 
 # get rid of this for python2.6+
 try:
@@ -37,3 +32,101 @@ except ImportError:
         json = Dummy()
         json.loads = cjson.decode
         json.dumps = cjson.encode
+
+def download(url, **kwargs):
+    code, body = real_download(url, **kwargs)
+    if code == 503:
+        sleep = kwargs.get('sleep', 0)
+        if sleep == 9:
+            raise CommError("Too many 503 replies", url)
+        else:
+            time.sleep(2**sleep)
+            kwargs['sleep'] = sleep + 1
+            return download(url, **kwargs)
+    elif str(code)[0] != '2':
+        raise CommError("Invalid HTTP reply (expected 200 or 206 got %s)" %
+            code, url, code)
+    return body
+
+def open_local(path, url):
+    # XXX: wouldn't it be polite to give a specific error message if this
+    # operation fails
+    fd = file(path, "r", BUFFER_SIZE)
+    sze = os.stat(path).st_size
+    return (fd, sze, "file://" + path)
+
+def open_remote(url, expect = 200):
+    conn = Conn(url)
+    return conn, conn.length(), url   
+
+class Conn(object):
+    def __init__(self, url):
+        self.url = url
+        self.buf = None
+        self.offset = 0
+        self.orig_offset = 0
+        self.eof = False
+        self.header = {}
+        self.size = None
+        self.read(1)
+        self.i = 0
+    
+    def close(self):
+        pass
+
+    def read(self, n = None):
+        if n == None:
+            return self.read_all()
+        if self.buf == None or self.i >= len(self.buf):
+            if self.eof:
+                return ''
+            self.i = 0
+            if self.size:
+                end = min(self.size, self.offset + CHUNK_SIZE) - 1
+            else:
+                end = self.offset + CHUNK_SIZE - 1
+            self.buf = download(self.url, header = self.header,
+                offset = (self.offset, end))
+            if 'content-range' in self.header:
+                self.size = int(self.header['content-range'].split('/')[1])
+            else:
+                self.size = int(self.header['content-length'])
+            self.orig_offset = self.offset
+            self.offset += len(self.buf)
+            if self.size and self.offset >= self.size:
+                self.eof = True
+            elif self.buf == "":
+                self.eof = True
+        ret = self.buf[self.i:self.i + n]
+        self.i += len(ret)
+        return ret
+
+    def getheader(self, header):
+        return self.header.get(header, None)
+
+    def length(self):
+        return self.size
+
+    def read_all(self):
+        buf = cStringIO.StringIO()
+        ret = True
+        while ret:
+            ret = self.read(CHUNK_SIZE)
+            buf.write(ret)
+        return buf.getvalue()
+
+    def tell(self):
+        return self.orig_offset + self.i
+
+    def seek(self, pos, mode = 0):
+        bef = self.offset
+        if mode == 0:
+            self.offset = pos
+        elif mode == 1:
+            self.offset = self.tell() + pos
+        else:
+            self.offset = self.size - pos
+        self.eof = False
+        self.buf = None
+        self.orig_offset = self.offset
+        self.i = 0
--- pydisco/disco/comm_httplib.py
+++ pydisco/disco/comm_httplib.py
@@ -2,81 +2,47 @@ import httplib, urllib2
 
 from disco.error import CommError
 
-FILE_BUFFER_SIZE = 10 * 1024**2
 MAX_RETRIES = 10
-http_pool = {}
 
-def download(url, data = None, redir = False, offset = 0, method = None):
-    if redir:
-        req = urllib2.Request(url, data)
-        if offset:
-            req.add_header("Range", "bytes=%d-" % offset)
+def download(url, data = None, offset = 0, method = None,
+        sleep = 0, header = {}):
+    while True:
         try:
-            c = urllib2.urlopen(req)
-        except urllib2.HTTPError, x:
-            raise CommError(x.msg, url)
-        r = c.read()
-        c.close()
-        return r
-    fd, sze, url = open_remote(url, data = data,
-        offset = offset, method = method)
-    return fd.read()
-
-
-def check_code(fd, expected, url):
-    if fd.status != expected:
-        raise CommError("Invalid HTTP reply (expected %s got %s)" %\
-                (expected, fd.status), url, fd.status)
-
-def open_remote(url, data = None, expect = 200, offset = 0,
-        ttl = MAX_RETRIES, method = None):
-    http = None
-    try:
-        ext_host, ext_file = url[7:].split("/", 1)
-        ext_file = "/" + ext_file
-
-        # We can't open a new HTTP connection for each intermediate
-        # result -- this would result to M * R TCP connections where
-        # M is the number of maps and R the number of reduces. Instead,
-        # we pool connections and reuse them whenever possible. HTTP
-        # 1.1 defaults to keep-alive anyway.
-        if ext_host in http_pool:
-            http = http_pool[ext_host]
-            if http._HTTPConnection__response:
-                http._HTTPConnection__response.read()
-        else:
+            ext_host, ext_file = url[7:].split("/", 1)
+            ext_file = "/" + ext_file
             http = httplib.HTTPConnection(ext_host)
-            http_pool[ext_host] = http
-
-        h = {}
-        if offset:
-            h = {"Range": "bytes=%d-" % offset}
-            expect = 206
-
-        if data:
-            meth = "POST"
-        elif method != None:
-            meth = method
-        else:
-            meth = "GET"
-        
-        http.request(meth, ext_file, data, headers = h)
-        fd = http.getresponse()
-        check_code(fd, expect, url)
-        sze = fd.getheader("content-length")
-        if sze:
-            sze = int(sze)
-        return (fd, sze, url)
-    except (httplib.HTTPException, httplib.socket.error), e:
-        if not ttl:
-            raise CommError("Downloading %s failed "
-                    "after %d attempts: %s" %
-                    (url, MAX_RETRIES, e), url)
-        if http:
-            http.close()
-        if ext_host in http_pool:
-            del http_pool[ext_host]
-        return open_remote(url, data, ttl=ttl - 1)
+            h = {}
+            if offset:
+                if type(offset) == tuple:
+                    offs = "bytes=%d-%d" % offset
+                else:
+                    offs = "bytes=%d-" % offset
+                h = {"Range": offs}
+            if data:
+                meth = "POST"
+            elif method != None:
+                meth = method
+            else:
+                meth = "GET"
+            http.request(meth, ext_file, data, headers = h)
+            fd = http.getresponse()
+            if fd.status == 302:
+                loc = fd.getheader("location")
+                if loc.startswith("http://"):
+                    url = loc
+                elif loc.startswith("/"):
+                    url = "http://%s%s" % (ext_host, loc)
+                else:
+                    url = "%s/%s" % (url, loc)
+                continue
+            header.update(fd.getheaders())
+            return fd.status, fd.read()
+        except (httplib.HTTPException, httplib.socket.error), e:
+            if not ttl:
+                raise CommError("Downloading %s failed "
+                        "after %d attempts: %s" %
+                        (url, MAX_RETRIES, e), url)
+            ttl -= 1
 
 def upload(fname, urls, retries = 10):
     raise CommError("Uploading with httplib is not currently supported. Install pycurl.", urls[0])
