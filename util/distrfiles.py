bdc8569073d05a02fb4a054b5faa23f0b4768912
Ruslan Spivak <ruslan.spivak@gmail.com>
2010-04-05 23:45:15 -0400

    Consistent string quoting.

--- util/distrfiles.py
+++ util/distrfiles.py
@@ -23,19 +23,19 @@ if len(sys.argv) < 3:
     log(USAGE_INFO)
     sys.exit(1)
 
-if "DISCO_ROOT" not in os.environ:
-    log("Specify DISCO_ROOT (data lives at $DISCO_ROOT/data)")
+if 'DISCO_ROOT' not in os.environ:
+    log('Specify DISCO_ROOT (data lives at $DISCO_ROOT/data)')
     sys.exit(1)
 
-root = os.environ["DISCO_ROOT"]
+root = os.environ['DISCO_ROOT']
 ssh = []
-if "SSH_KEY" in os.environ:
-    ssh += ["-i", os.environ["SSH_KEY"]]
+if 'SSH_KEY' in os.environ:
+    ssh += ['-i', os.environ['SSH_KEY']]
 
-user = os.environ.get("SSH_USER", "")
+user = os.environ.get('SSH_USER', '')
 
 files = os.listdir(sys.argv[1])
-if sys.argv[1][-1] == "/":
+if sys.argv[1][-1] == '/':
     name = os.path.basename(sys.argv[1][:-1])
 else:
     name = os.path.basename(sys.argv[1])
@@ -59,44 +59,44 @@ for l in file(sys.argv[2]):
 
 fno = int(math.ceil(max(1, float(len(files)) / len(nodes))))
 
-log("Dataset name: %s" % name)
-log("%d files and %d nodes: %d files per node" % (len(files), len(nodes), fno))
+log('Dataset name: %s' % name)
+log('%d files and %d nodes: %d files per node' % (len(files), len(nodes), fno))
 
 random.shuffle(nodes)
 nodes = iter(nodes)
 procs = []
 
 while True:
-    nset = [sys.argv[1] + "/" + f  for f  in files[:fno]]
+    nset = [sys.argv[1] + '/' + f  for f  in files[:fno]]
     if not nset:
         break
     files = files[fno:]
     node = unode = nodes.next().strip()
     if user:
-        unode = "%s@%s" % (user, node)
+        unode = '%s@%s' % (user, node)
 
-    if "REMOVE_FIRST" in os.environ:
-        call(["ssh"] + ssh + [unode, "rm -Rf %s/data/%s" % (root, name)])
+    if 'REMOVE_FIRST' in os.environ:
+        call(['ssh'] + ssh + [unode, 'rm -Rf %s/data/%s' % (root, name)])
 
-    if call(["ssh"] + ssh + [unode, "mkdir /%s/data/%s" % (root, name)]):
+    if call(['ssh'] + ssh + [unode, 'mkdir /%s/data/%s' % (root, name)]):
         log("Couldn't mkdir %s:%s/data/%s" % (node, root, name))
 
-    log("Copying %d files to %s" % (len(nset), node))
-    p = Popen("nice -19 %s scp %s %s %s:%s/data/%s/" %
-        (ionice, " ".join(ssh), " ".join(nset), unode, root, name),
+    log('Copying %d files to %s' % (len(nset), node))
+    p = Popen('nice -19 %s scp %s %s %s:%s/data/%s/' %
+        (ionice, ' '.join(ssh), ' '.join(nset), unode, root, name),
             shell = True)
     procs.append((node, p, nset))
 
-log("Waiting for copyers to finish..")
+log('Waiting for copyers to finish..')
 
 for node, p, nset in procs:
     if p.wait():
-        log("Copying to %s failed" % node)
+        log('Copying to %s failed' % node)
     else:
-        log("%s ok" % node)
-        print "\n".join(("disco://%s/%s/%s" %
+        log('%s ok' % node)
+        print '\n'.join(('disco://%s/%s/%s' %
             (hostname[node], name, os.path.basename(f))
                 for f in nset))
 
-log("Done")
+log('Done')
 
