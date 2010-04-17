discoproject/disco

commit d193064e9edf499b9dafa8e58636543b5f17f9ba

moved jobargs to job.run so you can construct a job without running (useful for getting a job handle for existing jobs). cleaned up eventmonitor and did something more sensible with poll_interval/timeout. fixed multijobtestfixture. closes #94.

pydisco/disco/core.py
@@ -8,6 +8,7 @@ from disco.error import DiscoError, JobError, CommError
 from disco.eventmonitor import EventMonitor
 from disco.modutil import find_modules
 from disco.netstring import decode_netstring_fd, encode_netstring_fd
+from disco.settings import DiscoSettings
 
 class Params(object):
     def __init__(self, **kwargs):
@@ -227,11 +228,9 @@ class Job(object):
                        'results',
                        'wait')
 
-    def __init__(self, master, name='', **kwargs):
+    def __init__(self, master, name):
         self.master  = master
         self.name    = name
-        self.jobdict = JobDict(prefix=name, **kwargs)
-        self._run()
 
     def __getattr__(self, attr):
         if attr in self.proxy_functions:
@@ -239,12 +238,13 @@ class Job(object):
             return partial(getattr(self.master, attr), self.name)
         raise AttributeError("%r has no attribute %r" % (self, attr))
 
-    def _run(self):
-        jobpack = self.jobdict.pack()
+    def run(self, **kwargs):
+        jobpack = JobDict(prefix=self.name, **kwargs).pack()
         reply = json.loads(self.master.request('/disco/job/new', jobpack))
         if reply[0] != 'ok':
             raise DiscoError("Failed to start a job. Server replied: " + reply)
         self.name = reply[1]
+        return self
 
 class Disco(object):
     def __init__(self, master):
@@ -274,7 +274,7 @@ class Disco(object):
         return json.loads(self.request('/disco/ctrl/nodeinfo'))
 
     def joblist(self):
-        return json.loads(self.request("/disco/ctrl/joblist"))
+        return json.loads(self.request('/disco/ctrl/joblist'))
 
     def blacklist(self, node):
         self.request('/disco/ctrl/blacklist', '"%s"' % node)
@@ -306,8 +306,8 @@ class Disco(object):
             stats.add(Stats(self.oob_get(name, s)))
         return stats
 
-    def new_job(self, **kwargs):
-        return Job(self, **kwargs)
+    def new_job(self, name, **kwargs):
+        return Job(self, name=name).run(**kwargs)
 
     def kill(self, name):
         self.request('/disco/ctrl/kill_job', '"%s"' % name)
@@ -327,7 +327,7 @@ class Disco(object):
     def jobspec(self, name):         # XXX: deprecate this
         return self.jobdict(name)
 
-    def events(self, name, offset = 0):
+    def events(self, name, offset=0):
         def event_iter(events):
             offs = offset
             lines = events.splitlines()
@@ -342,24 +342,21 @@ class Disco(object):
                     # Let's ensure that at least the last newline
                     # is always retrieved.
                     #if i == len(lines) - 1 and\
-                    #    events.endswith("\n"):
+                    #    events.endswith('\n'):
                     #    offs -= 1
                     yield offs, event
-        try:
-            r = self.request("/disco/ctrl/rawevents?name=%s" % name,
-                     redir = True,
-                     offset = offset)
-        except Exception, x:
-            return []
+        r = self.request('/disco/ctrl/rawevents?name=%s' % name,
+                         redir=True,
+                         offset=offset)
 
         if len(r) < 2:
             return []
         return event_iter(r)
 
-    def results(self, jobspec, timeout = 2000):
+    def results(self, jobspec, timeout=2000):
         jobspecifier = JobSpecifier(jobspec)
         data = json.dumps([timeout, list(jobspecifier.jobnames)])
-        results = json.loads(self.request("/disco/ctrl/get_results", data))
+        results = json.loads(self.request('/disco/ctrl/get_results', data))
 
         if type(jobspec) == str:
             return results[0][1]
@@ -373,10 +370,10 @@ class Disco(object):
         return others, active
 
     def jobinfo(self, name):
-        return json.loads(self.request("/disco/ctrl/jobinfo?name=%s" % name))
+        return json.loads(self.request('/disco/ctrl/jobinfo?name=%s' % name))
 
-    def check_results(self, name, start_time, timeout, poll_interval):
-        status, results = self.results(name, timeout = poll_interval)
+    def check_results(self, name, start_time, timeout):
+        status, results = self.results(name, timeout=timeout)
         if status == 'ready':
             return results
         if status != 'active':
@@ -385,13 +382,19 @@ class Disco(object):
             raise JobError("Timeout", self.master, name)
         raise Continue()
 
-    def wait(self, name, show = None, poll_interval = 2, timeout = None, clean = False):
-        event_monitor = EventMonitor(show, disco=self, name=name)
+    def wait(self, name,
+             poll_interval=2,
+             timeout=2000,
+             clean=False,
+             show=DiscoSettings()['DISCO_EVENTS']):
+        event_monitor = EventMonitor(Job(self, name=name),
+                                     format=show,
+                                     poll_interval=poll_interval)
         start_time    = time.time()
         while True:
             event_monitor.refresh()
             try:
-                return self.check_results(name, start_time, timeout, poll_interval * 1000)
+                return self.check_results(name, start_time, timeout)
             except Continue:
                 continue
             finally:

pydisco/disco/eventmonitor.py
@@ -127,23 +127,17 @@ class JSONEventWriter(EventWriter):
 
 
 class EventMonitor(object):
-    def __init__(self, show, disco = None, name = None, job = None):
-        if job:
-            self.disco, self.name = job.master, job.name
-        else:
-            self.disco, self.name = disco, name
-
-        if not self.disco:
-            raise DiscoError("Specify either job or disco and name")
-
-        self.offset      = 0
-        self.prev_status = None
-        self.output      = OutputStream(format=(show or os.getenv('DISCO_EVENTS')))
-        self.output.write(message=self.name)
+    def __init__(self, job, format=None, poll_interval=2):
+        self.job           = job
+        self.offset        = 0
+        self.poll_interval = poll_interval
+        self.prev_status   = None
+        self.output        = OutputStream(format)
+        self.output.write(message=self.job.name)
 
     @property
     def events(self):
-        return self.disco.events(self.name, self.offset)
+        return self.job.events(self.offset)
 
     @property
     def isenabled(self):
@@ -151,7 +145,7 @@ class EventMonitor(object):
 
     @property
     def stats(self):
-        jobinfo = self.disco.jobinfo(self.name)
+        jobinfo = self.job.jobinfo()
         if sum(jobinfo['redi'][1:]):
             return ['reduce'] + jobinfo['redi']
         return ['map'] + jobinfo['mapi']
@@ -172,4 +166,4 @@ class EventMonitor(object):
             if self.prev_status != status:
                 self.output.write(status=status)
                 self.prev_status = status
-            time.sleep(2)
+            time.sleep(self.poll_interval)

pydisco/disco/test.py
@@ -198,10 +198,14 @@ class DiscoMultiJobTestFixture(DiscoJobTestFixture):
         return self.test_servers[m].urls(getattr(self, 'inputs_%d' % (m + 1)))
 
     def __getattribute__(self, name):
-        if name.startswith('results_') or name.startswith('result_reader_'):
-            attribute, n = name.rsplit('_', 1)
-            return getattr(self, attribute)(int(n) - 1)
-        return super(DiscoMultiJobTestFixture, self).__getattribute__(name)
+        try:
+            return super(DiscoMultiJobTestFixture, self).__getattribute__(name)
+        except AttributeError:
+            for prefix in ('input', 'results', 'result_reader'):
+                if name.startswith('%s_' % prefix):
+                    attribute, n = name.rsplit('_', 1)
+                    return getattr(self, attribute)(int(n) - 1)
+            raise
 
     def setUp(self):
         host, port    = self.test_server_address
@@ -213,14 +217,11 @@ class DiscoMultiJobTestFixture(DiscoJobTestFixture):
                                  getattr(self, 'getdata_%d' % n, self.getdata))
             self.test_servers[m].start()
             try:
-                if not hasattr(self, 'input_%d' % n):
-                    setattr(self, 'input_%d' % n, self.input(m))
-
                 jobargs = {'name': '%s_%d' % (self.__class__.__name__, n)}
                 for jobarg in self.jobargs:
-                    jobargname = '%s_%d' % (jobarg, n)
-                    if hasattr(self, jobargname):
-                        jobargs[jobarg] = getattr(self, jobargname)
+                    attr = getattr(self, '%s_%d' % (jobarg, n), None)
+                    if attr:
+                        jobargs[jobarg] = attr
 
                 self.jobs[m] = self.disco.new_job(**jobargs)
                 setattr(self, 'job_%d' % n, self.jobs[m])
@@ -239,7 +240,7 @@ class DiscoMultiJobTestFixture(DiscoJobTestFixture):
         for m in xrange(self.njobs):
             n = m + 1
             for result, answer in zip(getattr(self, 'results_%d' % n),
-                          getattr(self, 'answers_%d' % n)):
+                                      getattr(self, 'answers_%d' % n)):
                 self.assertEquals(result, answer)
 
 class DiscoTestLoader(TestLoader):

commit ecbba02ff7a94be7c1e0dedadba0d3e1afcd4da8

fixed two regressions introduced in d193064e9 in core.py

pydisco/disco/core.py
@@ -240,9 +240,12 @@ class Disco(object):
                     #    events.endswith('\n'):
                     #    offs -= 1
                     yield offs, event
-        r = self.request('/disco/ctrl/rawevents?name=%s' % name,
-                         redir=True,
-                         offset=offset)
+        try:
+            r = self.request("/disco/ctrl/rawevents?name=%s" % name,
+                     redir = True,
+                     offset = offset)
+        except Exception, x:
+            return []
 
         if len(r) < 2:
             return []
@@ -304,8 +307,8 @@ class Disco(object):
         """Returns a dictionary containing information about the job *name*."""
         return json.loads(self.request('/disco/ctrl/jobinfo?name=%s' % name))
 
-    def check_results(self, name, start_time, timeout):
-        status, results = self.results(name, timeout=timeout)
+    def check_results(self, name, start_time, timeout, poll_interval):
+        status, results = self.results(name, timeout=poll_interval)
         if status == 'ready':
             return results
         if status != 'active':
@@ -351,7 +354,8 @@ class Disco(object):
         while True:
             event_monitor.refresh()
             try:
-                return self.check_results(name, start_time, timeout)
+                return self.check_results(name, start_time,
+                                          timeout, poll_interval * 1000)
             except Continue:
                 continue
             finally: