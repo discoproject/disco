ecc8934a107dadc2aa411ae2a55e5d882f940943
Jared Flatow <jflatow@gmail.com>
2010-05-07 18:36:59 -0700

    allow overriding of globals by settings subclasses; only complain about non-existant settings file if user specified it

--- settings.py
+++ settings.py
@@ -7,20 +7,23 @@ class Settings(dict):
     Provides special mechanisms for setting and overriding default values.
     Defaults can be overridden by a settings file and/or the environment.
     """
-    settings_file_var = None
     defaults = {}
+    globals  = globals()
+    settings_file_var = None
 
     def __init__(self, **kwargs):
         super(Settings, self).__init__(kwargs)
         if self.settings_file_var:
-            execfile(self[self.settings_file_var], {}, self)
+            settings_file = self[self.settings_file_var]
+            if os.path.exists(settings_file):
+                execfile(settings_file, {}, self)
 
     def __getitem__(self, key):
         """Get the setting `key`, first checking the environment, then the instance."""
         if key in os.environ:
             return os.environ[key]
         if key not in self:
-            return eval(self.defaults[key], globals(), self)
+            return eval(self.defaults[key], self.globals, self)
         return super(Settings, self).__getitem__(key)
 
     def safedir(self, key):
