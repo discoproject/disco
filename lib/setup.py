17d80156ecb39cda6dbbf84c21081b07881e4c37
Jared Flatow <jflatow@gmail.com>
2010-05-11 22:27:58 -0700

    added url to setup.py

--- lib/setup.py
+++ lib/setup.py
@@ -5,5 +5,6 @@ from distutils.core import setup
 setup(name='disco',
       version='0.1',
       description='An open-source mapreduce framework.',
+      url='http://discoproject.org',
       author='Nokia Research Center',
       packages=['disco', 'clx'])
