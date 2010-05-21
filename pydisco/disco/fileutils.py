discoproject/disco

commit 47def9761bb9327cd4514276370fd0e917ca9899

make consistent types of errors raised; closes #120

pydisco/disco/fileutils.py
@@ -2,7 +2,7 @@
 import sys, time, os, cPickle, cStringIO, struct, zlib
 import errno, fcntl
 
-from disco.util import data_err, err, msg
+from disco.error import DataError
 
 class DiscoOutput(object):
     VERSION = 1
@@ -131,10 +131,8 @@ def _safe_fileop(op, mode, outfile, timeout):
                 outstream.close()
                 return r
             except Exception, x:
-                # output file is inconsistent state
-                # we must crash the job
-                err("Updating file %s failed: %s" %\
-                    (outfile, x))
+                # output file is inconsistent state, but might be recoverable
+                raise DataError("Updating file failed: %s" % (x), outfile)
         except IOError, x:
             # Python / BSD doc guides us to check for these errors
             if x.errno in (errno.EACCES, errno.EAGAIN, errno.EWOULDBLOCK):
@@ -142,7 +140,7 @@ def _safe_fileop(op, mode, outfile, timeout):
                 timeout -= 0.1
             else:
                 raise
-    data_err("Timeout when updating file %s" % outfile, outfile)
+    raise DataError("Timeout when updating file", outfile)
 
 
 def ensure_file(fname, data = None, timeout = 60, mode = 500):
@@ -163,9 +161,8 @@ def ensure_file(fname, data = None, timeout = 60, mode = 500):
                 time.sleep(1)
                 timeout -= 1
             else:
-                data_err("Writing external file %s failed"\
-                     % fname, fname)
-    data_err("Timeout in writing external file %s" % fname, fname)
+                raise DataError("Writing external file failed", fname)
+    raise DataError("Timeout in writing external file", fname)
 
 
 def write_files(ext_data, path):
@@ -175,7 +172,6 @@ def write_files(ext_data, path):
         # make sure that no files are written outside the given path
         p = os.path.abspath(os.path.join(path, fname))
         if os.path.dirname(p) == path:
-            ensure_file(path + "/" + fname, data = data)
+            ensure_file(path + "/" + fname, data=data)
         else:
-            err("Unsafe filename %s" % fname)
-
+            raise ValueError("Unsafe filename %s" % fname)

pydisco/disco/error.py
@@ -41,7 +41,7 @@ class DataError(DiscoError):
     These errors are treated specially by Disco master in that they are assumed to be recoverable.
     If Disco thinks an error is recoverable, it will retry the task on another node.
     """
-    def __init__(self, msg, url, code = None):
+    def __init__(self, msg, url, code=None):
         self.msg = msg
         self.url = url
         self.code = code

pydisco/disco/func.py
@@ -64,7 +64,7 @@ These functions are provided by Disco to help :class:`disco.core.Job` creation:
 .. autofunction:: reduce_output_stream
 """
 import re, cPickle
-from disco.util import err, data_err, msg
+from disco.error import DataError
 
 def map(entry, params):
     """
@@ -223,24 +223,23 @@ def old_netstr_reader(fd, size, fname, head = ''):
 
         i = data.find(" ", idx, idx + 11)
         if i == -1:
-            data_err("Corrupted input (%s). Could not "\
-                   "parse a value length at %d bytes."\
-                    % (fname, tot), fname)
+            raise DataError("Corrupted input: "\
+                            "Could not parse a value length at %d bytes."\
+                            % (tot), fname)
         else:
             lenstr = data[idx:i + 1]
             idx = i + 1
 
         if ldata < i + 1:
-            data_err("Truncated input (%s). "\
-                "Expected %d bytes, got %d" %\
-                (fname, size, tot), fname)
+            raise DataError("Truncated input: "\
+                            "Expected %d bytes, got %d" % (size, tot), fname)
 
         try:
             llen = int(lenstr)
         except ValueError:
-            data_err("Corrupted input (%s). Could not "\
-                "parse a value length at %d bytes."\
-                    % (fname, tot), fname)
+            raise DataError("Corrupted input: "\
+                            "Could not parse a value length at %d bytes."\
+                            % (tot), fname)
 
         tot += len(lenstr)
 
@@ -252,10 +251,9 @@ def old_netstr_reader(fd, size, fname, head = ''):
         msg = data[idx:idx + llen]
 
         if idx + llen + 1 > ldata:
-            data_err("Truncated input (%s). "\
-                "Expected a value of %d bytes "\
-                "(offset %u bytes)" %\
-                (fname, llen + 1, tot), fname)
+            raise DataError("Truncated input: "\
+                            "Expected a value of %d bytes (offset %u bytes)"\
+                            % (llen + 1, tot), fname)
 
         tot += llen + 1
         idx += llen + 1
@@ -327,17 +325,14 @@ def re_reader(item_re_str, fd, size, fname, output_tail=False, read_buffer_size=
 
         if not len(r) or (size!=None and tot >= size):
             if size != None and tot < size:
-                data_err("Truncated input (%s). "\
-                     "Expected %d bytes, got %d" %\
-                     (fname, size, tot), fname)
+                raise DataError("Truncated input: "\
+                "Expected %d bytes, got %d" % (size, tot), fname)
             if len(buf):
                 if output_tail:
                     yield [buf]
                 else:
-                    msg("Couldn't match the last %d "\
-                        "bytes in %s. Some bytes may be "\
-                        "missing from input." %\
-                        (len(buf), fname))
+                    print "Couldn't match the last %d bytes in %s. "\
+                    "Some bytes may be missing from input." % (len(buf), fname)
             break
 
 
@@ -491,7 +486,7 @@ def disco_input_stream(stream, size, url, ignore_corrupt = False):
             is_compressed, checksum, chunk_size =\
                 struct.unpack('<BIQ', stream.read(13))
         except:
-            data_err("Truncated data at %d bytes" % offset)
+            raise DataError("Truncated data at %d bytes" % offset, url)
         if not chunk_size:
             return
         chunk = stream.read(chunk_size)
@@ -499,11 +494,11 @@ def disco_input_stream(stream, size, url, ignore_corrupt = False):
         try:
             data = zlib.decompress(chunk) if is_compressed else chunk
             if checksum != (zlib.crc32(data) & 0xFFFFFFFF):
-                raise Exception()
-        except:
+                raise ValueError("Checksum does not match")
+        except (ValueError, zlib.error), e:
             if not ignore_corrupt:
-                data_err("Corrupted data between bytes %d-%d" %
-                    (offset, offset + chunk_size), url)
+                raise DataError("Corrupted data between bytes %d-%d: %s" %
+                                (offset, offset + chunk_size, e), url)
         offset += chunk_size
         chunk = cStringIO.StringIO(data)
         while True:
@@ -513,11 +508,3 @@ def disco_input_stream(stream, size, url, ignore_corrupt = False):
                 break
 
 chain_reader = netstr_reader = disco_input_stream
-
-
-
-
-
-
-
-

commit 7832b9e9623e40d86b7a2f2b3ede413ba45608ca

fixed slightly erroneous error handling in safe_fileop introduced by 47def97 - ensure that enough free disk space is available before safe_fileop

pydisco/disco/fileutils.py
@@ -4,6 +4,8 @@ import errno, fcntl
 
 from disco.error import DataError
 
+MIN_DISK_SPACE = 1024**2
+
 class DiscoOutput(object):
     VERSION = 1
     def __init__(self, stream, compress_level, min_chunk, version):
@@ -121,17 +123,14 @@ def safe_update(outfile, lines, timeout = 60):
     return _safe_fileop(update, "a+", outfile, timeout = timeout)
 
 def _safe_fileop(op, mode, outfile, timeout):
+    ensure_free_space(outfile)
     outstream = file(outfile, mode)
     while timeout > 0:
         try:
             fcntl.flock(outstream, fcntl.LOCK_EX | fcntl.LOCK_NB)
-            try:
-                r = op(outstream)
-                outstream.close()
-                return r
-            except Exception, x:
-                # output file is inconsistent state, but might be recoverable
-                raise DataError("Updating file failed: %s" % (x), outfile)
+            r = op(outstream)
+            outstream.close()
+            return r
         except IOError, x:
             # Python / BSD doc guides us to check for these errors
             if x.errno in (errno.EACCES, errno.EAGAIN, errno.EWOULDBLOCK):
@@ -174,3 +173,12 @@ def write_files(ext_data, path):
             ensure_file(path + "/" + fname, data=data)
         else:
             raise ValueError("Unsafe filename %s" % fname)
+
+def ensure_free_space(fname):
+    s = os.statvfs(fname)
+    free = s.f_bsize * s.f_bavail
+    if free < MIN_DISK_SPACE:
+        raise DataError("Only %d KB disk space available. Task failed." % (free / 1024), fname)
+
+
+