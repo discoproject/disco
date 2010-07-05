discoproject/disco

commit 9cc88ab163e938efdbad965519fcb5b86ee05409

Fix some issues reported by dialyzer.

master/src/ddfs/ddfs_node.erl
@@ -119,7 +119,7 @@ handle_call({put_tag_data, {Tag, Data}}, _From, S) ->
         "tag", S#state.root, Vol),
     case ddfs_util:ensure_dir(Local) of
         ok ->
-            F = filename:join(Local, ["!partial.", binary_to_list(Tag)]),
+            F = filename:join([Local, "!partial.", binary_to_list(Tag)]),
             {reply, case prim_file:write_file(F, Data) of
                 ok -> {ok, Vol};
                 {error, _} = E -> E
@@ -135,7 +135,7 @@ handle_call({put_tag_commit, Tag, TagVol}, _, S) ->
     {TagName, Time} = ddfs_util:unpack_objname(Tag),
 
     TagL = binary_to_list(Tag),
-    Src = filename:join(Local, ["!partial.", TagL]),
+    Src = filename:join([Local, "!partial.", TagL]),
     Dst = filename:join(Local,  TagL),
     case ddfs_util:safe_rename(Src, Dst) of
         ok ->

master/src/ddfs/ddfs_put.erl
@@ -61,7 +61,7 @@ receive_blob(Req, {Path, Fname}, Url) ->
     case prim_file:read_file_info(Dir) of
         {error, enoent} ->
             Tstamp = ddfs_util:timestamp(),
-            Dst = filename:join(Path, ["!partial-", Tstamp, ".", Fname]),
+            Dst = filename:join([Path, "!partial-", Tstamp, ".", Fname]),
             case file:open(Dst, [write, raw, binary]) of
                 {ok, IO} -> receive_blob(Req, IO, Dst, Url);
                 Error -> error_reply(Req, "Opening file failed", Dst, Error)

master/src/disco_main.erl
@@ -10,7 +10,6 @@
 
 -export([init/1, start/2, stop/1]).
 
-write_pid(false) -> ok;
 write_pid(PidFile) ->
     case file:write_file(PidFile, os:getpid()) of
         ok -> ok;

master/src/disco_web.erl
@@ -43,8 +43,6 @@ reply({ok, Data}, Req) ->
     Req:ok({"application/json", [], mochijson2:encode(Data)});
 reply({raw, Data}, Req) ->
     Req:ok({"text/plain", [], Data});
-reply({relo, Loc}, Req) ->
-    Req:respond({302, [{"Location", Loc}], <<>>});
 reply({file, File, Docroot}, Req) ->
     Req:serve_file(File, Docroot);
 reply(not_found, Req) ->

commit f8303edd41c13c5e023faf36ccc3f48f15c0ae1e

fixed filename:join regressions introduced by 9cc88ab1

master/src/ddfs/ddfs_node.erl
@@ -119,7 +119,7 @@ handle_call({put_tag_data, {Tag, Data}}, _From, S) ->
         "tag", S#state.root, Vol),
     case ddfs_util:ensure_dir(Local) of
         ok ->
-            F = filename:join([Local, "!partial.", binary_to_list(Tag)]),
+            F = filename:join(Local, ["!partial.", binary_to_list(Tag)]),
             {reply, case prim_file:write_file(F, Data) of
                 ok -> {ok, Vol};
                 {error, _} = E -> E
@@ -135,7 +135,7 @@ handle_call({put_tag_commit, Tag, TagVol}, _, S) ->
     {TagName, Time} = ddfs_util:unpack_objname(Tag),
 
     TagL = binary_to_list(Tag),
-    Src = filename:join([Local, "!partial.", TagL]),
+    Src = filename:join(Local, ["!partial.", TagL]),
     Dst = filename:join(Local,  TagL),
     case ddfs_util:safe_rename(Src, Dst) of
         ok ->

master/src/ddfs/ddfs_put.erl
@@ -67,7 +67,7 @@ receive_blob(Req, {Path, Fname}, Url) ->
     case prim_file:read_file_info(Dir) of
         {error, enoent} ->
             Tstamp = ddfs_util:timestamp(),
-            Dst = filename:join([Path, "!partial-", Tstamp, ".", Fname]),
+            Dst = filename:join(Path, ["!partial-", Tstamp, ".", Fname]),
             case file:open(Dst, [write, raw, binary]) of
                 {ok, IO} -> receive_blob(Req, IO, Dst, Url);
                 Error -> error_reply(Req, "Opening file failed", Dst, Error)

master/src/ddfs/ddfs_util.erl
@@ -80,7 +80,7 @@ to_hex_helper(Int, L) ->
             D = Int div 16,
             R = Int rem 16,
             C = if R < 10 -> $0 + R;
-                   true   -> $A + R - 10
+                   true   -> $a + R - 10
                 end,
             to_hex_helper(D, [C|L])
     end.