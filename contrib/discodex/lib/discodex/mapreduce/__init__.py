ee72785b43265ba455634c38e91dc502666d594f
Jared Flatow <jflatow@gmail.com>
2010-07-19 12:05:27 -0700

    discodex: kvgroup no longer needed by index reduce

--- contrib/discodex/lib/discodex/mapreduce/__init__.py
+++ contrib/discodex/lib/discodex/mapreduce/__init__.py
@@ -45,8 +45,8 @@ class Indexer(DiscodexJob):
     @staticmethod
     def reduce(iterator, out, params):
         # there should be a discodb writer of some sort
-        from discodb import DiscoDB, kvgroup
-        DiscoDB(kvgroup(iterator)).dump(out)
+        from discodb import DiscoDB
+        DiscoDB(iterator).dump(out)
 
     def reduce_output_stream(stream, partition, url, params):
         return stream, 'discodb:%s' % url.split(':', 1)[1]
