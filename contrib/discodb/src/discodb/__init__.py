b73c959a3a5c1195dd9f1be046b9f3cff3a3ae03
Jared Flatow <jflatow@gmail.com>
2010-07-21 16:26:36 -0700

    discodb: added DiscoDict: in-memory DiscoDB-like object (key -> values)

--- contrib/discodb/src/discodb/__init__.py
+++ contrib/discodb/src/discodb/__init__.py
@@ -1,7 +1,8 @@
 from ._discodb import DiscoDB, DiscoDBError
+from .discodict import DiscoDict
 from .metadb import MetaDB
 from .query import Q
 from .tools import kvgroup
 
-__all__ = ['DiscoDB', 'DiscoDBError', 'Q', 'kvgroup', 'MetaDB']
+__all__ = ['DiscoDB', 'DiscoDBError', 'DiscoDict', 'Q', 'kvgroup', 'MetaDB']
 
