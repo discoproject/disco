17f2c8b8cfcbaefaddefce23c38163c679d2ea4b
Jared Flatow <jflatow@gmail.com>
2010-07-21 17:11:24 -0700

    discodb: can now pickle DiscoDB objects normally

--- contrib/discodb/src/discodb/__init__.py
+++ contrib/discodb/src/discodb/__init__.py
@@ -6,3 +6,12 @@ from .tools import kvgroup
 
 __all__ = ['DiscoDB', 'DiscoDBError', 'DiscoDict', 'Q', 'kvgroup', 'MetaDB']
 
+from copy_reg import pickle
+
+def unpickle(string):
+    return DiscoDB.loads(string)
+
+def __reduce__(discodb):
+    return unpickle, (discodb.dumps(), )
+
+pickle(DiscoDB, __reduce__)
