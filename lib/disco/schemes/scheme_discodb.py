e36f4eb3ef735ef4af225d5f47e72def7bc3f49e
Jared Flatow <jflatow@gmail.com>
2010-08-05 11:10:23 -0700

    remove the ugly Task.discodb hack from scheme_discodb

--- lib/disco/schemes/scheme_discodb.py
+++ lib/disco/schemes/scheme_discodb.py
@@ -8,15 +8,15 @@ def input_stream(fd, size, url, params):
     path, rest   = rest.split('!', 1) if '!' in rest else (rest, '')
 
     if netloc[0] == Task.netloc[0]:
-        Task.discodb = DiscoDB.load(open(os.path.join(Task.root, path)))
+        discodb = DiscoDB.load(open(os.path.join(Task.root, path)))
     else:
-        Task.discodb = DiscoDB.loads(download('disco://%s/%s' % (netloc, path)))
+        discodb = DiscoDB.loads(download('disco://%s/%s' % (netloc, path)))
 
     if rest:
         method, arg = rest.split('/', 1)
         if method == 'query':
             if hasattr(params, 'discodb_query'):
-                return Task.discodb.query(params.discodb_query), size, url
-            return Task.discodb.query(Q.urlscan(arg)), size, url
-        return getattr(Task.discodb, method)(), size, url
-    return Task.discodb, size, url
+                return discodb.query(params.discodb_query), size, url
+            return discodb.query(Q.urlscan(arg)), size, url
+        return getattr(discodb, method)(), size, url
+    return discodb, size, url
