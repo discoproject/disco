98e483535653f8357f61434b019018b447beef16
Jared Flatow <jflatow@gmail.com>
2010-08-09 14:55:06 -0700

    added useful example for using Job classes / chunking

---
+++ examples/util/chunk.py
@@ -0,0 +1,32 @@
+"""
+An example using Job classes to implement chunking.
+
+Could be run as follows (assuming this module can be found on sys.path):
+
+disco run --param tag chunks:tag chunk.LineChunker /path/to/textfile
+
+or
+
+disco run --param tag chunks:tag chunk.LineChunker existing-tag-containing-textfiles
+"""
+from disco.core import Job, Params
+
+class LineChunker(Job):
+    params = Params(tag=None)
+
+    def _map_input_stream(fd, size, url, params):
+        from disco.ddfs import DDFS
+        tag = params.tag or 'disco:chunks:%s' % Task.jobname
+        yield url, DDFS(Task.master).chunk(tag, [url])
+    map_input_stream = [_map_input_stream]
+
+    def map(entry, params):
+        yield entry
+
+class GzipLineChunker(LineChunker):
+    def _map_input_stream(fd, size, url, params):
+        from disco.ddfs import DDFS
+        from disco.func import gzip_reader
+        tag = params.tag or 'disco:chunks:%s' % Task.jobname
+        yield url, DDFS(Task.master).chunk(tag, [url], reader=gzip_reader)
+    map_input_stream = [_map_input_stream]
