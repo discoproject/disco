425a60db86315b32e59b165ae55893efefd01ec0
Ville Tuulos <ville.h.tuulos@nokia.com>
2010-08-28 00:47:16 -0700

    SaveOnlyMapTestCase added

--- tests/test_save.py
+++ tests/test_save.py
@@ -1,7 +1,35 @@
-from disco.test import DiscoMultiJobTestFixture, DiscoTestCase
+from disco.test import DiscoJobTestFixture, DiscoMultiJobTestFixture
+from disco.test import DiscoTestCase
 from disco.func import chain_reader
 from disco.util import ddfs_name
 from disco.ddfs import DDFS
+
+class SaveOnlyMapTestCase(DiscoJobTestFixture, DiscoTestCase):
+    inputs = map(str, range(10))
+    partitions = None
+    save = True
+
+    def getdata(self, path):
+        return '%s\n' % path
+
+    @staticmethod
+    def map(e, params):
+        return [(e + "#", '')]
+
+    @property
+    def answers(self):
+        return [('%s#' % e, '') for e in self.inputs]
+
+    def runTest(self):
+        results = sorted(list(self.results))
+        ddfs = DDFS(self.disco_master_url)
+        tag = self.disco.results(self.job.name)[1][0]
+        self.assertEquals(len(list(ddfs.blobs(tag))), len(self.inputs))
+        self.assertEquals(self.answers, results)
+
+    def tearDown(self):
+        super(SaveOnlyMapTestCase, self).tearDown()
+        DDFS(self.disco_master_url).delete(ddfs_name(self.job.name))
 
 class SaveTestCase(DiscoMultiJobTestFixture, DiscoTestCase):
     njobs = 2
