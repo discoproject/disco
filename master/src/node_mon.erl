23a376773665b8a77aca757823e3fc30175d676f
Prashanth Mundkur <prashanth.mundkur@nokia.com>
2010-11-16 19:56:09 -0800

    Retry connections to epmd on failure, since assuming a local node to be remote has much worse consequences than the reverse.

--- master/src/node_mon.erl
+++ master/src/node_mon.erl
@@ -4,6 +4,7 @@
 -define(RESTART_DELAY, 600000).
 -define(SLAVE_ARGS, "+K true -connect_all false").
 -define(RPC_CALL_TIMEOUT, 30000).
+-define(RPC_RETRY_TIMEOUT, 120000).
 
 start_link(Host) ->
     spawn_link(fun() -> spawn_node(Host) end).
@@ -93,14 +94,16 @@ slave_start(Host) ->
 -spec is_master(nonempty_string()) -> bool().
 is_master(Host) ->
     % the underlying tcp connection used by net_adm:names() may hang,
-    % so we use a timed rpc.  if the call times out, we assume Host is
-    % remote, and hence cannot be the master.
+    % so we use a timed rpc.
     case rpc:call(node(), net_adm, names, [Host], ?RPC_CALL_TIMEOUT) of
         {ok, Names} ->
             Master = string:sub_word(atom_to_list(node()), 1, $@),
             lists:keymember(Master, 1, Names);
         _ ->
-            false
+            % retry the connection, after a while.
+            error_logger:warning_report({"net_adm:names() timed out", Host}),
+            timer:sleep(?RPC_RETRY_TIMEOUT),
+            is_master(Host)
     end.
 
 -spec start_temp_gc(node(), nonempty_string()) -> pid().
