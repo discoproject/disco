2a900310881d678c8e56a1353afd50dc281d1920
Jared Flatow <jflatow@gmail.com>
2010-12-06 11:53:44 -0800

    docs: override DDFS signature so it doesnt print all settings

--- lib/disco/ddfs.py
+++ lib/disco/ddfs.py
@@ -8,6 +8,9 @@ See also: :ref:`DDFS`.
 
         Parameters below which are indicated as tags can be specified as
         a `tag://` URL, or the name of the tag.
+
+.. autoclass:: DDFS(master=None, proxy=None, settings=DiscoSettings())
+        :members:
 """
 import os, re, random
 from cStringIO import StringIO
