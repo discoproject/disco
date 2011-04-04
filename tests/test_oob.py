fb5dab1f1330cea2a464f9a27463d3918549bf7c
Jared Flatow <jflatow@gmail.com>
2011-04-04 16:26:21 -0700

    convert test_oob

--- tests/test_oob.py
+++ tests/test_oob.py
@@ -1,89 +1,64 @@
-from disco.test import DiscoMultiJobTestFixture
-from disco.test import DiscoJobTestFixture, DiscoTestCase
-from disco.core import Params
+import string
 
-import string, sys
+from disco.job import JobChain
+from disco.test import TestCase, TestJob
+from disco.util import load_oob
 
-class OOBTestCase(DiscoMultiJobTestFixture, DiscoTestCase):
-    njobs    = 2
-    inputs_1     = list(string.ascii_lowercase)
-    inputs_2     = ['raw://a', 'raw://b', 'raw://c']
-    partitions_1 = 10
-
-    @property
-    def params_2(self):
-        return Params(job=self.job_1.name)
-
-    def getdata_1(self, path):
-        return path
+class OOBJob1(TestJob):
+    partitions = 10
 
     @staticmethod
-    def map_1(e, params):
+    def map(e, params):
         v = 'value:%s' % e
         put(e, v)
-        return [(e, v)]
+        yield e, v
 
     @staticmethod
-    def reduce_1(iter, out, params):
+    def reduce(iter, params):
         for k, v in iter:
             assert v == get(k)
         x = 'reduce:%d' % this_partition()
         put(x, 'value:%s' % x)
-        out.add('all', 'ok')
+        yield 'all', 'ok'
 
-    @property
-    def input_2(self):
-        self.job_1.wait()
-        return self.inputs_2
-
+class OOBJob2(TestJob):
     @staticmethod
-    def map_2(e, params):
-        from disco.util import load_oob
-        x = load_oob(Task.master, params.job, e)
+    def map(e, params):
+        x = load_oob(Task.master, params['job'], e)
         assert x == 'value:%s' % e
-        return [('good', '')]
+        yield 'good', ''
 
-    def runTest(self):
-        super(OOBTestCase, self).runTest()
-        self.assertEquals(sorted(self.job_1.oob_list()), sorted(self.oob_data))
-
-    @property
-    def answers_1(self):
-        return [('all', 'ok')] * self.partitions_1
-
-    @property
-    def answers_2(self):
-        return [('good', '')] * len(self.inputs_2)
-
-    @property
-    def oob_data(self):
-        for i in self.inputs_1:
-            yield '%s' % i
-        for i in xrange(self.partitions_1):
-            yield 'reduce:%s' % i
-
-
-class LargeOOBTestCase(DiscoJobTestFixture, DiscoTestCase):
-    @property
-    def input(self):
-        return ['raw://%d' % i for i in range(self.num_workers)]
-
+class LargeOOBJob(TestJob):
     @staticmethod
     def map(e, params):
         for i in range(10):
-            put("%s-%d" % (e, i), "val:%s-%d" % (e, i))
+            put('%s-%d' % (e, i), 'val:%s-%d' % (e, i))
         return []
 
-    def runTest(self):
-        super(LargeOOBTestCase, self).runTest()
-        self.assertEquals(self.oob_data(), sorted(
-            [(key, self.job.oob_get(key)) for key in self.job.oob_list()]))
+class OOBTestCase(TestCase):
+    def serve(self, path):
+        return path
 
-    @property
-    def answers(self):
-        return []
+    def test_chain(self):
+        a, b = OOBJob1(), OOBJob2()
+        self.job = JobChain({a: [], b: []})
+        a.run(input=self.test_server.urls(list(string.ascii_lowercase))).wait()
+        b.run(input=['raw://a', 'raw://b', 'raw://c'],
+              params={'job': a.name})
+        self.assertResults(b, [('good', '')] * 3)
+        self.assertEquals(sorted(a.oob_list()),
+                          sorted(list(string.ascii_lowercase) +
+                                 ['reduce:%s' % i for i in xrange(a.partitions)]))
 
-    def oob_data(self):
-        return sorted([("%d-%d" % (i, j), "val:%d-%d" % (i, j))
-            for i in range(self.num_workers) for j in range(10)])
+class LargeOOBTestCase(TestCase):
+    def test_large(self):
+        self.job = LargeOOBJob().run(input=['raw://%d' % i
+                                            for i in range(self.num_workers)])
+        self.assertResults(self.job, [])
+        self.assertEquals(sorted((key, self.job.oob_get(key))
+                                 for key in self.job.oob_list()),
+                          sorted(('%d-%d' % (i, j), 'val:%d-%d' % (i, j))
+                                 for i in range(self.num_workers)
+                                 for j in range(10)))
+
 
