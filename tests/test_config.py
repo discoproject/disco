5e835dd9c980120eb899c4ab965aec449c813c73
Jared Flatow <jflatow@gmail.com>
2011-04-04 15:25:33 -0700

    move test_nodechanges -> test_config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,7 @@
+import time
+
 from disco.test import TestCase, TestJob
+from disco.util import shuffled
 
 def unique_nodename(nodenames, count=0):
     nodename = 'missingnode_%s' % count
@@ -9,32 +12,51 @@ def unique_nodename(nodenames, count=0):
 class ConfigJob(TestJob):
     @staticmethod
     def map(e, params):
-        import time
         time.sleep(0.5)
-        yield int(e), ''
+        yield e, ''
 
-class LocalhostTestCase(TestCase):
-    def config(self, _config):
-        return [['localhost', '1']]
+class ConfigTestCase(TestCase):
+    def checkAnswers(self, job, input):
+        self.assertEquals(sorted(self.results(job)),
+                          sorted((str(i), '') for i in input))
+
+    def configTest(self, config):
+        input = range(self.num_workers * 2)
+        self.disco.config = config
+        self.job = ConfigJob().run(input=self.test_server.urls(input))
+        self.checkAnswers(self.job, input)
 
     def serve(self, path):
         return path
 
     def setUp(self):
-        super(LocalhostTestCase, self).setUp()
-        self._config = self.disco.config
-        self.disco.config = self.config(self._config)
+        super(ConfigTestCase, self).setUp()
+        self.config = self.disco.config
 
-    def runTest(self):
-        X = xrange(self.num_workers * 2)
-        self.job = ConfigJob().run(input=self.test_server.urls(X))
-        self.assertEqual(sum(k for k, v in self.results(self.job)), sum(X))
+    def test_changes(self):
+        if len(self.nodes) < 2:
+            self.skipTest("Cannot test node changes with < 2 nodes")
+        else:
+            local = ['url://%s' % node
+                     for node, max_workers in self.nodes.iteritems()
+                     for x in xrange(max_workers * 2)]
+            input = shuffled(local + range(self.num_workers))
+            self.job = ConfigJob().run(input=self.test_server.urls(input))
+            time.sleep(5)
+            self.disco.config = self.config[:2]
+            time.sleep(5)
+            self.disco.config = self.config[:1]
+            time.sleep(5)
+            self.disco.config = self.config
+            self.checkAnswers(self.job, input)
+
+    def test_localhost(self):
+        self.configTest([['localhost', '1']])
+
+    def test_missing_node(self):
+        nodenames = set(name for name, workers in self.config)
+        self.configTest(self.config + [[unique_nodename(nodenames), '1']])
 
     def tearDown(self):
-        super(LocalhostTestCase, self).tearDown()
-        self.disco.config = self._config
-
-class MissingNodeTestCase(LocalhostTestCase):
-    def config(self, _config):
-        nodenames = set(name for name, workers in _config)
-        return _config + [[unique_nodename(nodenames), '1']]
+        super(ConfigTestCase, self).tearDown()
+        self.disco.config = self.config
