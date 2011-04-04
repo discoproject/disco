6e75e5034193b968aaf7749ec9cf0e249520c467
Jared Flatow <jflatow@gmail.com>
2011-04-04 14:26:48 -0700

    convert test_modutil

--- tests/test_modutil.py
+++ tests/test_modutil.py
@@ -1,5 +1,5 @@
 from disco import modutil
-from disco.test import DiscoJobTestFixture, DiscoTestCase
+from disco.test import TestCase, TestJob
 from disco.error import ModUtilImportError
 
 import sys, os
@@ -18,14 +18,25 @@ def missing_module():
 def recursive_module():
     mod1.plusceil(1, 2)
 
-class ModUtilLocalTestCase(DiscoTestCase):
+class ModUtilJob(TestJob):
+    @staticmethod
+    def map(e, params):
+        x, y = [float(x) for x in e.split('|')]
+        yield mod1.plusceil(x, y) + math.ceil(1.5), ''
+
+class ModUtilTestCase(TestCase):
+    def serve(self, path):
+        return '%s\n' % path
+
     def setUp(self):
+        super(ModUtilTestCase, self).setUp()
         self.sys_path = sys.path
         self.support  = os.path.join(os.path.dirname(__file__), 'support')
         sys.path.append(self.support)
         os.environ['PYTHONPATH'] += ':%s' % self.support
 
     def tearDown(self):
+        super(ModUtilTestCase, self).tearDown()
         sys.path = self.sys_path
 
     def assertFindsModules(self, functions, modules, send_modules=False, recurse=True):
@@ -52,36 +63,17 @@ class ModUtilLocalTestCase(DiscoTestCase):
     def test_norecursive(self):
         self.assertFindsModules([recursive_module], ['mod1'], recurse=False)
 
-class ModUtilTestCase(DiscoJobTestFixture, DiscoTestCase):
-    inputs = ['0.5|1.2']
+    def test_auto_modules(self):
+        self.job = ModUtilJob().run(input=self.test_server.urls(['0.5|1.2']))
+        self.assertResults(self.job, [(4.0, '')])
 
-    def getdata(self, path):
-        return '%s\n' % path
+    def test_find_modules(self):
+        self.job = ModUtilJob()
+        self.job.run(input=self.test_server.urls(['0.5|1.2']),
+                     required_modules=modutil.find_modules([self.job.map]))
+        self.assertResults(self.job, [(4.0, '')])
 
-    @staticmethod
-    def map(e, params):
-        x, y = [float(x) for x in e.split('|')]
-        return [(mod1.plusceil(x, y) + math.ceil(1.5), '')]
-
-    @property
-    def answers(self):
-        yield 4.0, ''
-
-    def setUp(self):
-        self.sys_path = sys.path
-        self.support  = os.path.join(os.path.dirname(__file__), 'support')
-        sys.path.append(self.support)
-        os.environ['PYTHONPATH'] += ':%s' % self.support
-        super(ModUtilTestCase, self).setUp()
-
-    def tearDown(self):
-        sys.path = self.sys_path
-        super(ModUtilTestCase, self).tearDown()
-
-class AnotherModUtilTestCase(ModUtilTestCase):
-    @property
-    def required_modules(self):
-        return modutil.find_modules([self.map])
-
-class YetAnotherModUtilTestCase(ModUtilTestCase):
-    required_modules = ['math', 'mod1', 'mod2']
+    def test_list_modules(self):
+        self.job = ModUtilJob().run(input=self.test_server.urls(['0.5|1.2']),
+                                    required_modules=['math', 'mod1', 'mod2'])
+        self.assertResults(self.job, [(4.0, '')])
