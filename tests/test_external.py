62b96e6f0b0d1ee1bccded93cbb6fbfd9c8e5dcc
Jared Flatow <jflatow@gmail.com>
2011-04-04 11:29:39 -0700

    convert test_external; (not tested)

--- tests/test_external.py
+++ tests/test_external.py
@@ -1,45 +1,41 @@
-from disco.test import DiscoJobTestFixture, DiscoTestCase
+from disco.test import TestCase, TestJob
 from disco.util import external
 
 from subprocess import check_call, STDOUT
 from os import uname, path
 
-class ExternalTestCase(DiscoJobTestFixture, DiscoTestCase):
-    inputs     = ["ape", "cat", "dog"]
+class ExternalJob(TestJob):
     ext_params = {"test1": "1,2,3", "one two three": "dim\ndam\n", "dummy": "value"}
-    sort       = False
-    ext_map    = True
+    sort = False
 
-    def setUp(self):
+    @staticmethod
+    def reduce(iter, params):
+        for k, v in iter:
+            yield "red_" + k, "red_" + v
+
+class ExternalTestCase(TestCase):
+    inputs = ['ape', 'cat', 'dog']
+
+    def serve(self, path):
+        return 'test_%s\n' % path
+
+    def runTest(self):
         if uname()[0] == 'Darwin':
             self.skipTest('Cannot build static test_external on OS X')
         else:
             home = self.settings['DISCO_HOME']
+            test = path.join(home, 'tests', 'test_external')
             check_call(['gcc', '-g', '-O3', '-static', '-Wall',
                         '-I', path.join(home, 'ext'),
-                        '-o', path.join(home, 'tests', 'test_external'),
+                        '-o', test,
                         path.join(home, 'ext', 'disco.c'),
                         path.join(home, 'tests', 'test_external.c'),
                         '-l', 'Judy'],
                        stderr=STDOUT)
-            self.map = external([path.join(home, 'tests', 'test_external')])
-            super(ExternalTestCase, self).setUp()
-
-    @staticmethod
-    def reduce(iter, out, params):
-        for k, v in iter:
-            out.add("red_" + k, "red_" + v)
-
-    def getdata(self, path):
-        return 'test_%s\n' % path
-
-    @property
-    def answers(self):
-        yield 'discoapi', ''
-
-    def runTest(self):
-        results = sorted((v, k) for k, v in self.results)
-        for n, (v, k) in enumerate(results):
-            self.assertEquals(k, 'red_dkey')
-            self.assertEquals(v, 'red_test_%s\n' % self.inputs[n / 3])
-        self.assertEquals(len(results), 9)
+            self.job = ExternalJob().run(input=self.test_server.urls(self.inputs),
+                                         map=external([test]))
+            results = sorted((v, k) for k, v in self.results(self.job))
+            for n, (v, k) in enumerate(results):
+                self.assertEquals(k, 'red_dkey')
+                self.assertEquals(v, 'red_test_%s\n' % self.inputs[n / 3])
+            self.assertEquals(len(results), 9)
