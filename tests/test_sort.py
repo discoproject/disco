18a809ba8e8a03ac378f7757ddf60302f0022c23
Jared Flatow <jflatow@gmail.com>
2011-04-04 20:01:22 -0700

    convert test_sort

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,29 +1,27 @@
 import base64, string
-from disco.test import DiscoJobTestFixture, DiscoTestCase
 
-class SortTestCase(DiscoJobTestFixture, DiscoTestCase):
-    inputs     = [''] * 100
-    sort       = True
+from disco.test import TestCase, TestJob
+from disco.util import kvgroup, shuffled
 
-    def getdata(self, path):
-        return '\n'
+alphanum = list(string.ascii_letters) + map(str, xrange(10))
 
-    @staticmethod
-    def map(e, params):
-        letters  = sorted(string.ascii_lowercase * 10)
-        numbers  = [str(i) for i in range(10)] * 10
-        sequence = [(base64.encodestring(s), '') for s in letters + numbers]
-        random.shuffle(sequence)
-        return sequence
+class SortJob(TestJob):
+    sort = True
 
     @staticmethod
-    def reduce(iter, out, params):
-        d = set()
-        for k, vs in disco.util.kvgroup(iter):
-            assert k not in d, "Key %s seen already, sorting failed." % k
-            d.add(k)
-            out.add(base64.decodestring(k), len(list(vs)))
+    def map(string, params):
+        return shuffled((base64.encodestring(c), '') for c in string * 10)
 
-    @property
-    def answers(self):
-        return sorted((str(k), 1000) for k in list(string.ascii_lowercase) + range(10))
+    @staticmethod
+    def reduce(iter, params):
+        for k, vs in kvgroup(iter):
+            yield base64.decodestring(k), len(list(vs))
+
+class SortTestCase(TestCase):
+    def serve(self, path):
+        return ''.join(alphanum)
+
+    def runTest(self):
+        self.job = SortJob().run(input=self.test_server.urls([''] * 100))
+        self.assertResults(self.job, sorted((c, 1000) for c in alphanum))
+
