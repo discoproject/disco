bf1c902d2dd60a240f40802035c7b491e8bdf401
pmundkur <prashanth.mundkur@gmail.com>
2011-04-08 23:28:07 -0700

    bugfix: ensure ascii-encoding of raw url content

--- lib/disco/schemes/scheme_raw.py
+++ lib/disco/schemes/scheme_raw.py
@@ -6,5 +6,6 @@ def input_stream(fd, sze, url, params):
     from cStringIO import StringIO
     from disco.util import schemesplit
     scheme, string = schemesplit(url)
-    return (StringIO(string), len(string), url)
+    ascii = string.encode('ascii')
+    return (StringIO(ascii), len(ascii), url)
 
