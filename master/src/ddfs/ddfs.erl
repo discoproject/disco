f8736646dbd72893ab9a687bc1e8a19b46718649
pmundkur <prashanth.mundkur@gmail.com>
2011-04-11 19:44:24 -0700

    dialyzer: fix an incorrect spec and confusing variable name

--- master/src/ddfs/ddfs.erl
+++ master/src/ddfs/ddfs.erl
@@ -6,17 +6,19 @@
 -export([new_blob/4, tags/2, get_tag/4, update_tag/5, update_tag_delayed/5,
          replace_tag/5, delete/3, delete_attrib/4]).
 
--spec new_blob(node(), string(), non_neg_integer(), [node()]) ->
+-type gen_server() :: pid() | atom() | {atom(), node()}.
+
+-spec new_blob(gen_server(), string(), non_neg_integer(), [node()]) ->
     'invalid_name' | 'too_many_replicas' | {'ok', [string()]} | _.
-new_blob(Host, Blob, Replicas, Exclude) ->
+new_blob(Server, Blob, Replicas, Exclude) ->
     validate(Blob, fun() ->
         Obj = [Blob, "$", ddfs_util:timestamp()],
-        gen_server:call(Host, {new_blob, Obj, Replicas, Exclude})
+        gen_server:call(Server, {new_blob, Obj, Replicas, Exclude})
     end).
 
--spec tags(node(), binary()) -> {'ok', [binary()]}.
-tags(Host, Prefix) ->
-    case gen_server:call(Host, {get_tags, safe}, ?NODEOP_TIMEOUT) of
+-spec tags(gen_server(), binary()) -> {'ok', [binary()]}.
+tags(Server, Prefix) ->
+    case gen_server:call(Server, {get_tags, safe}, ?NODEOP_TIMEOUT) of
         {ok, Tags} ->
             {ok, if Prefix =:= <<>> ->
                 Tags;
@@ -26,45 +28,45 @@ tags(Host, Prefix) ->
         E -> E
     end.
 
--spec get_tag(node(), nonempty_string(), atom() | string(), token() | 'internal') ->
+-spec get_tag(gen_server(), nonempty_string(), atom() | string(), token() | 'internal') ->
     'invalid_name' | {'missing', _} | 'unknown_attribute'
     | {'ok', binary()} | {'error', _}.
-get_tag(Host, Tag, Attrib, Token) ->
-    tagop(Host, Tag, {get, Attrib, Token}, ?NODEOP_TIMEOUT).
+get_tag(Server, Tag, Attrib, Token) ->
+    tagop(Server, Tag, {get, Attrib, Token}, ?NODEOP_TIMEOUT).
 
--spec update_tag(node(), nonempty_string(), [[binary()]], token(), [term()]) -> _.
-update_tag(Host, Tag, Urls, Token, Opt) ->
-    tagop(Host, Tag, {update, Urls, Token, Opt}).
+-spec update_tag(gen_server(), nonempty_string(), [[binary()]], token(), [term()]) -> _.
+update_tag(Server, Tag, Urls, Token, Opt) ->
+    tagop(Server, Tag, {update, Urls, Token, Opt}).
 
--spec update_tag_delayed(node(), nonempty_string(), [[binary()]],
+-spec update_tag_delayed(gen_server(), nonempty_string(), [[binary()]],
                          token(), [term()]) -> _.
-update_tag_delayed(Host, Tag, Urls, Token, Opt) ->
-    tagop(Host, Tag, {delayed_update, Urls, Token, Opt}).
+update_tag_delayed(Server, Tag, Urls, Token, Opt) ->
+    tagop(Server, Tag, {delayed_update, Urls, Token, Opt}).
 
--spec replace_tag(node(), nonempty_string(),
+-spec replace_tag(gen_server(), nonempty_string(),
                   attrib(), [binary()] | [[binary()]], token()) -> _.
-replace_tag(Host, Tag, Field, Value, Token) ->
-    tagop(Host, Tag, {put, Field, Value, Token}).
+replace_tag(Server, Tag, Field, Value, Token) ->
+    tagop(Server, Tag, {put, Field, Value, Token}).
 
--spec delete_attrib(node(), nonempty_string(), attrib(), token()) -> _.
-delete_attrib(Host, Tag, Field, Token) ->
-    tagop(Host, Tag, {delete_attrib, Field, Token}).
+-spec delete_attrib(gen_server(), nonempty_string(), attrib(), token()) -> _.
+delete_attrib(Server, Tag, Field, Token) ->
+    tagop(Server, Tag, {delete_attrib, Field, Token}).
 
--spec delete(node(), nonempty_string(), token() | 'internal') -> term().
-delete(Host, Tag, Token) ->
-    tagop(Host, Tag, {delete, Token}).
+-spec delete(gen_server(), nonempty_string(), token() | 'internal') -> term().
+delete(Server, Tag, Token) ->
+    tagop(Server, Tag, {delete, Token}).
 
--spec tagop(node(), nonempty_string(),
+-spec tagop(gen_server(), nonempty_string(),
             {'delete', _}
             | {'delete_attrib', _, _}
             | {'delayed_update', _, _, _}
             | {'update', _, _, _}
             | {'put', _, _, _}) -> term().
-tagop(Host, Tag, Op) ->
-    tagop(Host, Tag, Op, ?TAG_UPDATE_TIMEOUT).
-tagop(Host, Tag, Op, Timeout) ->
+tagop(Server, Tag, Op) ->
+    tagop(Server, Tag, Op, ?TAG_UPDATE_TIMEOUT).
+tagop(Server, Tag, Op, Timeout) ->
     validate(Tag, fun() ->
-                      gen_server:call(Host,
+                      gen_server:call(Server,
                                       {tag, Op, list_to_binary(Tag)}, Timeout)
                   end).
 
