9dfc572acc31cf57119fcc8305aa17f2d72d9a0f
Ville Tuulos <ville.h.tuulos@nokia.com>
2011-04-14 14:03:02 -0700

    improved test_ratelimit

--- tests/test_ratelimit.py
+++ tests/test_ratelimit.py
@@ -1,7 +1,7 @@
 from disco.test import TestCase, TestJob
 from disco.error import JobError
 
-class PrintJob(TestJob):
+class BadJob(TestJob):
     @staticmethod
     def map(e, params):
         import sys, disco.json
@@ -9,13 +9,36 @@ class PrintJob(TestJob):
         sys.stderr.write('MSG %d %s\n' % (len(msg), msg))
         return []
 
+class GoodJob(TestJob):
+    @staticmethod
+    def map(e, params):
+        import sys, disco.json
+        print e
+        return [(e.strip(), '')]
+
+
 class RateLimitTestCase(TestCase):
+    num_lines = 100
     inputs = [1]
 
     def serve(self, path):
-        return 'badger\n' * 1000
+        return 'badger\n' * self.num_lines
 
-    def test_print(self):
-        self.job = PrintJob().run(input=self.test_server.urls([1]))
+    def answers(self):
+        return [(l, '') for l in self.serve('').splitlines()]
+
+    def messages(self):
+        for offset, (tstamp, host, msg) in self.disco.events(self.job.name):
+            yield msg
+
+    def test_bad(self):
+        self.job = BadJob().run(input=self.test_server.urls([1]))
         self.assertRaises(JobError, self.job.wait)
         self.assertEquals(self.job.jobinfo()['active'], 'dead')
+
+    def test_good(self):
+        self.job = GoodJob().run(input=self.test_server.urls([1]))
+        self.assertResults(self.job, self.answers())
+        num_messages = sum(1 for msg in self.messages() if 'badger' in msg)
+        self.assertEquals(num_messages, self.num_lines)
+
