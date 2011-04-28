ddbdd14cc2db484854d9fbf268a3e5320e8a81c3
Jared Flatow <jflatow@gmail.com>
2011-04-28 13:30:29 -0700

    fix dPickle to only handle the funcs/partials and leave the rest to pickle

--- lib/disco/dPickle.py
+++ lib/disco/dPickle.py
@@ -1,38 +1,19 @@
-import cPickle, cStringIO, functools, marshal, pickle, types
+import functools, marshal, pickle, types
 
 from cPickle import loads
+from cStringIO import StringIO
 
 def dumps(obj, protocol=None):
-    file = cStringIO.StringIO()
+    file = StringIO()
     Pickler(file, protocol).dump(obj)
     return file.getvalue()
 
-def pack(object):
-    if hasattr(object, 'func_code'):
-        if object.func_closure != None:
-            raise TypeError("Function must not have closures: "
-                            "%s (try using functools.partial instead)"
-                            % object.func_name)
-        return marshal.dumps((object.func_code, object.func_defaults))
-    if isinstance(object, (list, tuple)):
-        object = type(object)(pack(o) for o in object)
-    return cPickle.dumps(object, cPickle.HIGHEST_PROTOCOL)
+def unfunc(packed, globals={'__builtins__': __builtins__}):
+    code, defs = marshal.loads(packed)
+    return types.FunctionType(code, globals, argdefs=defs)
 
-def unpack(string, globals={'__builtins__': __builtins__}):
-    try:
-        object = cPickle.loads(string)
-        if isinstance(object, (list, tuple)):
-            return type(object)(unpack(s, globals=globals) for s in object)
-        return object
-    except Exception:
-        try:
-            code, defs = marshal.loads(string)
-            return types.FunctionType(code, globals, argdefs=defs)
-        except Exception, e:
-            raise ValueError("Could not unpack: %s (%s)" % (string, e))
-
-def unpartial(string):
-    func, args, kwds = unpack(string)
+def unpartial(packed):
+    func, args, kwds = loads(packed)
     return functools.partial(func, *args, **kwds)
 
 class Pickler(pickle.Pickler):
@@ -42,11 +23,12 @@ class Pickler(pickle.Pickler):
         if func.__module__.startswith('disco.'):
             self.save_global(func)
         else:
-            self.save_reduce(unpack, (pack(func),), obj=func)
+            packed = marshal.dumps((func.func_code, func.func_defaults))
+            self.save_reduce(unfunc, (packed,), obj=func)
     dispatch[types.FunctionType] = save_func
 
     def save_partial(self, partial):
-        parts = partial.func, partial.args, partial.keywords or {}
-        self.save_reduce(unpartial, (pack(parts),), obj=partial)
+        packed = dumps((partial.func, partial.args, partial.keywords or {}))
+        self.save_reduce(unpartial, (packed,), obj=partial)
     dispatch[functools.partial] = save_partial
 
