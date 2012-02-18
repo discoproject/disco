2e9e296b5ddd09e9f47856c747efa6dbf2f28fa7
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2012-02-17 18:11:45 -0800

    Add lager dep to erl path.

--- master/src/node_mon.erl
+++ master/src/node_mon.erl
@@ -74,7 +74,7 @@ slave_env() ->
                    [io_lib:format(" -pa ~s/ebin/~s", [Home, Dir])
                     || Dir <- [""]],
                    [io_lib:format(" -pa ~s/deps/~s/ebin", [Home, Dir])
-                    || Dir <- ["mochiweb"]],
+                    || Dir <- ["mochiweb", "lager"]],
                    [io_lib:format(" -env ~s '~s'", [S, disco:get_setting(S)])
                     || S <- disco:settings()]]).
 
