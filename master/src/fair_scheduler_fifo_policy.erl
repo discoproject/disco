f089a8e2e16ddcd26b646185a4fed0a4010987c9
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2012-05-10 15:31:59 -0700

    Tidy code in the scheduler policies.

--- master/src/fair_scheduler_fifo_policy.erl
+++ master/src/fair_scheduler_fifo_policy.erl
@@ -1,43 +1,67 @@
+% This module implements a scheduler policy for the global task
+% scheduler (GTS) in fair_scheduler.erl.  This module implements a
+% FIFO scheduling policy, where new jobs are added to the end of a
+% queue, and the job at the head of the queue is always selected as
+% the scheduling candidate.  Jobs only leave the queue when they
+% complete or die.
 
 -module(fair_scheduler_fifo_policy).
 -behaviour(gen_server).
 
--export([start_link/0, init/1, handle_call/3, handle_cast/2,
-    handle_info/2, terminate/2, code_change/3]).
+-include("disco.hrl").
+-include("gs_util.hrl").
+-include("fair_scheduler_policy.hrl").
 
+-export([start_link/0, init/1, handle_call/3, handle_cast/2,
+         handle_info/2, terminate/2, code_change/3]).
+
+-type state() :: queue().
+
+-spec start_link() -> {ok, pid()}.
 start_link() ->
     lager:info("Fair scheduler: FIFO policy"),
     case gen_server:start_link({local, sched_policy},
-            fair_scheduler_fifo_policy, [],
-            disco:debug_flags("fair_scheduler_fifo_policy")) of
-        {ok, Server} -> {ok, Server};
+                               fair_scheduler_fifo_policy, [],
+                               disco:debug_flags("fair_scheduler_fifo_policy"))
+    of
+        {ok, _Server} = Ret -> Ret;
         {error, {already_started, Server}} -> {ok, Server}
     end.
 
+-spec init(_) -> {ok, state()}.
 init(_) ->
     {ok, queue:new()}.
 
+-type cast_msgs() :: public_cast_msgs().
+
+-spec handle_cast(cast_msgs(), state()) -> gs_noreply().
 handle_cast({update_nodes, _}, Q) ->
     {noreply, Q};
-
 handle_cast({new_job, JobPid, JobName}, Q) ->
     erlang:monitor(process, JobPid),
     {noreply, queue:in({JobPid, JobName}, Q)}.
 
-handle_call({next_job, NotJobs}, _, Q) ->
-    {reply, dropwhile(Q, NotJobs), Q};
 
+-spec handle_call(current_priorities_msg(), from(), state()) ->
+                         gs_reply([{jobname(), priority()}]);
+                 (dbg_state_msg(), from(), state()) -> gs_reply(state());
+                 (next_job_msg(), from(), state()) -> gs_reply(next_job()).
 handle_call(current_priorities, _, Q) ->
     {reply, {ok, case queue:to_list(Q) of
-        [{_, N}|R] ->
-            [{N, -1.0}|[{M, 1.0} || {_, M} <- R]];
-        [] -> []
-    end}, Q};
+                     [{_, N}|R] ->
+                         [{N, -1.0}|[{M, 1.0} || {_, M} <- R]];
+                     [] ->
+                         []
+                 end}, Q};
 
 handle_call(dbg_get_state, _, Q) ->
-    {reply, Q, Q}.
+    {reply, Q, Q};
 
-dropwhile(Q, NotJobs) ->
+handle_call({next_job, NotJobs}, _, Q) ->
+    {reply, dropwhile(Q, NotJobs), Q}.
+
+-spec dropwhile(state(), [pid()]) -> next_job().
+ dropwhile(Q, NotJobs) ->
     case queue:out(Q) of
         {{value, {JobPid, _}}, NQ} ->
             V = lists:member(JobPid, NotJobs),
@@ -49,14 +73,15 @@ dropwhile(Q, NotJobs) ->
         {empty, _} -> nojobs
     end.
 
+-spec handle_info({'DOWN', _, _, pid(), _}, state()) -> gs_noreply().
 handle_info({'DOWN', _, _, JobPid, _}, Q) ->
     L = queue:to_list(Q),
     {value, {_, JobName} = E} = lists:keysearch(JobPid, 1, L),
     gen_server:cast(scheduler, {job_done, JobName}),
     {noreply, queue:from_list(L -- [E])}.
 
-% unused
+-spec terminate(term(), state()) -> ok.
+terminate(_Reason, _State) -> ok.
 
-terminate(_Reason, _State) -> {}.
-
+-spec code_change(term(), state(), term()) -> {ok, state()}.
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
