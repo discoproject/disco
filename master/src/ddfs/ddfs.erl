d676172baad083020ff785efa4f82bb23975103d
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2012-05-29 16:41:17 -0700

    Minor spec cleanup in ddfs.

--- master/src/ddfs/ddfs.erl
+++ master/src/ddfs/ddfs.erl
@@ -1,5 +1,6 @@
 -module(ddfs).
 
+-include("gs_util.hrl").
 -include("config.hrl").
 -include("ddfs.hrl").
 -include("ddfs_tag.hrl").
@@ -7,9 +8,10 @@
 -export([new_blob/4, tags/2, get_tag/4, update_tag/5, update_tag_delayed/5,
          replace_tag/5, delete/3, delete_attrib/4]).
 
--type gen_server() :: pid() | atom() | {atom(), node()}.
+% dummy
+-type state() :: none.
 
--spec new_blob(gen_server(), string(), non_neg_integer(), [node()])
+-spec new_blob(server(), string(), non_neg_integer(), [node()])
               -> invalid_name | too_many_replicas | {ok, [string()]} | _.
 new_blob(Server, Blob, Replicas, Exclude) ->
     validate(Blob, fun() ->
@@ -17,7 +19,7 @@ new_blob(Server, Blob, Replicas, Exclude) ->
         gen_server:call(Server, {new_blob, Obj, Replicas, Exclude})
     end).
 
--spec tags(gen_server(), binary()) -> {ok, [binary()]}.
+-spec tags(server(), binary()) -> {ok, [binary()]}.
 tags(Server, Prefix) ->
     case gen_server:call(Server, {get_tags, safe}, ?NODEOP_TIMEOUT) of
         {ok, Tags} ->
@@ -29,34 +31,34 @@ tags(Server, Prefix) ->
         E -> E
     end.
 
--spec get_tag(gen_server(), nonempty_string(), atom() | string(), token() | internal)
+-spec get_tag(server(), nonempty_string(), atom() | string(), token() | internal)
              -> invalid_name | {missing, _} | unknown_attribute | {ok, binary()} | {error, _}.
 get_tag(Server, Tag, Attrib, Token) ->
     tagop(Server, Tag, {get, Attrib, Token}, ?NODEOP_TIMEOUT).
 
--spec update_tag(gen_server(), nonempty_string(), [[binary()]], token(), [term()]) -> _.
+-spec update_tag(server(), nonempty_string(), [[binary()]], token(), [term()]) -> _.
 update_tag(Server, Tag, Urls, Token, Opt) ->
     tagop(Server, Tag, {update, Urls, Token, Opt}).
 
--spec update_tag_delayed(gen_server(), nonempty_string(), [[binary()]],
+-spec update_tag_delayed(server(), nonempty_string(), [[binary()]],
                          token(), [term()]) -> _.
 update_tag_delayed(Server, Tag, Urls, Token, Opt) ->
     tagop(Server, Tag, {delayed_update, Urls, Token, Opt}).
 
--spec replace_tag(gen_server(), nonempty_string(),
+-spec replace_tag(server(), nonempty_string(),
                   attrib(), [binary()] | [[binary()]], token()) -> _.
 replace_tag(Server, Tag, Field, Value, Token) ->
     tagop(Server, Tag, {put, Field, Value, Token}).
 
--spec delete_attrib(gen_server(), nonempty_string(), attrib(), token()) -> _.
+-spec delete_attrib(server(), nonempty_string(), attrib(), token()) -> _.
 delete_attrib(Server, Tag, Field, Token) ->
     tagop(Server, Tag, {delete_attrib, Field, Token}).
 
--spec delete(gen_server(), nonempty_string(), token() | internal) -> term().
+-spec delete(server(), nonempty_string(), token() | internal) -> term().
 delete(Server, Tag, Token) ->
     tagop(Server, Tag, {delete, Token}).
 
--spec tagop(gen_server(), nonempty_string(),
+-spec tagop(server(), nonempty_string(),
             {delete, _}
             | {delete_attrib, _, _}
             | {delayed_update, _, _, _}
