ffc4e098a819e80b149fcd2a65b7d75e4f0354c9
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2012-07-05 12:28:52 -0700

    Core logic for new pipeline scheduler; allows linear multi-stage pipelines with backtracking.

---
+++ master/src/jc_utils.erl
@@ -0,0 +1,283 @@
+-module(jc_utils).
+
+-include("common_types.hrl").
+-include("disco.hrl").
+-include("pipeline.hrl").
+-include("job_coordinator.hrl").
+
+-export([stage_info_opt/2, stage_info/2, last_stage_task/3,
+         update_stage/3, update_stage_tasks/4,
+         task_info/2, update_task_info/3, task_spec/2, add_task_spec/3,
+         task_inputs/2, task_outputs/2,
+         input_info/2, update_input_info/3, add_input/3,
+         update_input_failures/2, find_usable_input_hosts/1,
+         collect_stagewise/5, wakeup_waiters/3]).
+
+% information about a stage which may not have yet run.
+-spec stage_info_opt(stage_name(), gb_tree()) -> none | stage_info().
+stage_info_opt(Stage, Info) ->
+    case gb_trees:lookup(Stage, Info) of
+        none -> none;
+        {value, Info} -> Info
+    end.
+
+% information about a stage which the caller guarantees exists.
+-spec stage_info(stage_name(), gb_tree()) -> stage_info().
+stage_info(Stage, SI) ->
+    gb_trees:get(Stage, SI).
+
+-spec update_stage(stage_name(), stage_info(), gb_tree()) -> gb_tree().
+update_stage(Stage, Info, SI) ->
+    gb_trees:enter(Stage, Info, SI).
+
+-type task_op() :: run | stop | done.
+-spec update_stage_tasks(stage_name(), task_id(), task_op(), gb_tree())
+                        -> gb_tree().
+update_stage_tasks(S, Id, Op, SI) ->
+    mod_stage_tasks(S, Id, Op, stage_info(S, SI), SI).
+mod_stage_tasks(S, Id, Op, #stage_info{running = R, done = D} = Info, SI) ->
+    {R1, D1} =
+        case Op of
+            run  -> {lists:usort([Id | R]), D};
+            stop -> {R -- [Id], D};
+            done -> {R -- [Id], lists:usort([Id | D])}
+        end,
+    update_stage(S, Info#stage_info{running = R1, done = D1}, SI).
+
+% If this is the last pending task in the stage in order for the stage
+% to be done.
+-spec last_stage_task(stage_name(), task_id(), gb_tree()) -> boolean().
+last_stage_task(Stage, TaskId, SI) ->
+    #stage_info{done = Done, all = All} = stage_info(Stage, SI),
+    ((length(Done) + 1 =:= All) and (not lists:member(TaskId, Done))).
+
+-spec is_running(task_id(), gb_tree(), gb_tree()) -> boolean().
+is_running(TaskId, Tasks, SI) ->
+    #task_info{spec = #task_spec{taskid = TaskId, stage = Stage}}
+        = task_info(TaskId, Tasks),
+    #stage_info{done = Done} = stage_info(Stage, SI),
+    lists:member(TaskId, Done).
+
+-spec is_waiting(task_id(), gb_tree()) -> boolean().
+is_waiting(TaskId, Tasks) ->
+    #task_info{depends = Depends} = task_info(TaskId, Tasks),
+    Depends =/= [].
+
+-spec task_info(input | task_id(), gb_tree()) -> task_info().
+task_info(TaskId, Tasks) ->
+    gb_trees:get(TaskId, Tasks).
+
+-spec task_spec(input, gb_tree()) -> input;
+               (task_id(), gb_tree()) -> task_spec().
+task_spec(TaskId, Tasks) ->
+    (task_info(TaskId, Tasks))#task_info.spec.
+
+-spec task_outputs(input | task_id(), gb_tree()) -> [task_output()].
+task_outputs(TaskId, Tasks) ->
+    (task_info(TaskId, Tasks))#task_info.outputs.
+
+% This should only be called for new tasks.
+-spec add_task_spec(task_id(), task_spec(), gb_tree()) -> gb_tree().
+add_task_spec(TaskId, TaskSpec, Tasks) ->
+    gb_trees:enter(TaskId, #task_info{spec = TaskSpec}, Tasks).
+
+-spec update_task_info(task_id(), task_info(), gb_tree()) -> gb_tree().
+update_task_info(TaskId, TInfo, Tasks) ->
+    gb_trees:enter(TaskId, TInfo, Tasks).
+
+% Input utilities.
+-spec add_input(input_id(), data_info(), gb_tree()) -> gb_tree().
+add_input(InputId, DInfo, DataMap) ->
+    gb_trees:enter(InputId, DInfo, DataMap).
+
+-spec input_info(input_id(), gb_tree()) -> data_info().
+input_info(InputId, DataMap) ->
+    gb_trees:get(InputId, DataMap).
+
+-spec update_input_info(input_id(), data_info(), gb_tree()) -> gb_tree().
+update_input_info(InputId, DInfo, DataMap) ->
+    gb_trees:enter(InputId, DInfo, DataMap).
+
+-spec task_inputs([input_id()], gb_tree()) -> [{input_id(), data_input()}].
+task_inputs(InputIds, DataMap) ->
+    [{Id, (gb_trees:get(Id, DataMap))#data_info.source} || Id <- InputIds].
+
+% Data utilities.
+-spec data_inputs(data_info()) -> [data_input()].
+data_inputs(#data_info{source = Source, locations = Locations}) ->
+    [Source | gb_trees:values(Locations)].
+-spec data_hosts(data_info()) -> [host()].
+data_hosts(DInfo) ->
+    lists:usort(lists:flatten([pipeline_utils:locations(DI)
+                               || DI <- data_inputs(DInfo)])).
+
+-spec update_input_failures([host()], data_info()) -> data_info().
+update_input_failures(Hosts, #data_info{failures = Failures} = DInfo) ->
+    F = lists:foldl(
+          fun(H, Fails) ->
+                  case gb_trees:lookup(H, Fails) of
+                      none -> Fails;
+                      {value, Cnt} -> gb_trees:enter(H, Cnt + 1, Fails)
+                  end
+          end, Failures, Hosts),
+    DInfo#data_info{failures = F}.
+
+-spec find_usable_input_hosts(data_info()) -> [host()].
+find_usable_input_hosts(#data_info{failures = Failures}) ->
+    [H || {H, Fails} <- gb_trees:to_list(Failures), Fails < ?MAX_INPUT_FAILURE].
+
+% Input regeneration and task re-run utilities.
+
+% Find the inputs that are local to the specified list of hosts.
+-spec local_inputs([input_id()], gb_set(), gb_tree()) -> [input_id()].
+local_inputs(InputIds, Hosts, DataMap) ->
+    local_inputs(InputIds, Hosts, DataMap, []).
+local_inputs([], _Hosts, _DataMap, Acc) ->
+    lists:usort(Acc);
+local_inputs([Id | Rest], Hosts, DataMap, Acc) ->
+    % An input is local to a set of hosts iff all its replicas are
+    % found only on those hosts.
+    Acc1 =
+        case lists:all(fun(H) -> gb_sets:is_member(H, Hosts) end,
+                       data_hosts(input_info(Id, DataMap)))
+        of  true ->  [Id | Acc];
+            false -> Acc
+        end,
+    local_inputs(Rest, Hosts, DataMap, Acc1).
+
+% The backtracking algorithm tries to figure out what tasks to run in
+% order to re-generate a specified intermediate output, given that the
+% output is not accessible on the host that it was generated on.
+
+% A precise algorithm to do this would involve the computation of
+% three sets:
+%
+% - Tasks, the set of tasks that need to be re-executed
+% - DataInputs, the set of data inputs that need to be re-generated
+% - Hosts, the set of hosts on which the Tasks cannot be run
+%
+% The sets are initialized as follows, given an intermediate input D
+% generated by a task T that is inaccessible on host H:
+%
+% Tasks := {T}, DataInputs := {D}, Hosts := {H}
+%
+% The sets are then closed under the following relations:
+%
+% (d) - if any input D' of any task T in Tasks is purely local to
+%       Hosts, then D' is in DataInputs
+%
+% (t) - if T is a task that generated a D in DataInputs, then T is in
+%       Tasks
+%
+% (h) - if H is a blacklisted host for any task T in Tasks, then H is
+%       in Hosts
+%
+% The sets forming the least-fixed-point (LFP) of the above relations
+% would provide the Tasks set sought for.
+%
+% However, the recursion between (d) and (h) makes this difficult to
+% compute, and in a dynamic environment with failing hosts, the
+% precision with respect to Hosts would not provide much benefit.
+%
+% Instead, we compute the LFP for (t) and (d), and keep Hosts fixed to
+% H along with the list of hosts that are currently down.  This leaves
+% the possibility that tasks will be re-executed on blacklisted hosts.
+
+% In the actual implementation, we actually don't need all the tasks
+% that need re-running, only the ones that can be re-run immediately
+% (i.e. have no dependencies).  Tasks that need re-running but have
+% dependencies on other tasks to be re-run, are just added as waiters
+% to those tasks.  The tasks that don't have dependencies form a
+% frontier that we collect as we traverse backwards, stage by stage,
+% through the dependency graph.  As a special case, if a task T is
+% already waiting or running, we don't recurse to its input generating
+% tasks.
+
+-spec collect_stagewise(task_id(), gb_tree(), gb_tree(), gb_tree(), gb_set())
+                       -> {gb_set(), gb_tree()}.
+collect_stagewise(TaskId, Tasks, SI, DataMap, FHosts) ->
+    collect_stagewise(gb_sets:from_list([TaskId]),  % Cur stage
+                      gb_sets:empty(),              % Prev stage
+                      gb_sets:empty(),              % Frontier
+                      Tasks, SI, DataMap, FHosts).
+
+collect_stagewise(CurStage, PrevStage, Frontier, Tasks, SI, DataMap, FHosts) ->
+    case {gb_sets:is_empty(CurStage), gb_sets:is_empty(PrevStage)} of
+        {true, true} ->
+            % We are done.
+            {Frontier, Tasks};
+        {true, false} ->
+            % We are done with the current stage; we recurse, making
+            % the previous stage the current one.
+            collect_stagewise(PrevStage, gb_sets:empty(), Frontier,
+                              Tasks, SI, DataMap, FHosts);
+        {false, _} ->
+            {TaskId, CurStage1} = gb_sets:take_largest(CurStage),
+            case is_running(TaskId, Tasks, SI) orelse is_waiting(TaskId, Tasks)
+            of  true ->
+                    % Nothing to do, we should have already updated
+                    % this task's waiters and depends.
+                    collect_stagewise(CurStage1, PrevStage, Frontier,
+                                      Tasks, SI, DataMap, FHosts);
+                false ->
+                    collect_task(TaskId, CurStage1, PrevStage, Frontier,
+                                 Tasks, SI, DataMap, FHosts)
+            end
+    end.
+
+collect_task(TaskId, CurStage, PrevStage, Frontier, Tasks, SI, DataMap, FHosts) ->
+    #task_info{spec = #task_spec{input = Inputs},
+               failed_hosts = TFHosts} = TInfo = task_info(TaskId, Tasks),
+    % Lookup task dependencies.
+    LocalInputs = local_inputs(Inputs, FHosts, DataMap),
+    GenTaskIds = lists:usort([TId || {TId, _} <- LocalInputs, TId =/= input]),
+    % Update task.  Its okay to overwrite depends since it should be
+    % empty, or else it would be waiting and we would not be
+    % processing it.
+    TInfo2 = TInfo#task_info{failed_hosts = gb_sets:union(TFHosts, FHosts),
+                             depends = GenTaskIds},
+    Tasks1 = update_task_info(TaskId, TInfo2, Tasks),
+    case GenTaskIds of
+        [] ->
+            % This task is runnable, and hence is on the frontier.
+            % Note that we might mark a task as such even though its
+            % inputs are pipeline inputs (task_id =:= input), and all
+            % of them are on FHosts.  There's not much we can do in
+            % this case, since we cannot regenerate pipeline inputs,
+            % so we optimistically run the task anyway.
+            Frontier1 = gb_sets:add_element(TaskId, Frontier),
+            collect_stagewise(CurStage, PrevStage, Frontier1,
+                              Tasks1, SI, DataMap, FHosts);
+        [_|_] ->
+            % These tasks belong to the previous stage, since we
+            % currently only have linear pipelines; and this task
+            % needs to wait on them (and hence is not in the
+            % frontier).
+            PrevStage1 = gb_sets:union(PrevStage, gb_sets:from_list(GenTaskIds)),
+            GenTasks = [{Id, task_info(Id, Tasks1)} || Id <- GenTaskIds],
+            Tasks2 =
+                lists:foldl(
+                  fun({Id, #task_info{waiters = W} = GT}, Tsks) ->
+                          UGT = GT#task_info{waiters = [TaskId|W]},
+                          gb_trees:enter(Id, UGT, Tsks)
+                  end, Tasks1, GenTasks),
+            collect_stagewise(CurStage, PrevStage1, Frontier,
+                              Tasks2, SI, DataMap, FHosts)
+    end.
+
+% This removes the completed task as a dependency from a set of
+% waiters, and returns the set of tasks that are now runnable since
+% they have no more dependencies, and the updated task set.
+-spec wakeup_waiters(task_id(), [task_id()], gb_tree()) -> {[task_id()], gb_tree()}.
+wakeup_waiters(TaskId, Waiters, Tasks) ->
+    lists:foldl(
+      fun(WId, {Runnable, Tsks}) ->
+              #task_info{depends = Deps} = WInfo = task_info(WId, Tsks),
+              UDeps = Deps -- [TaskId],
+              Runnable2 = case UDeps of
+                              [] -> [WId | Runnable];
+                              [_|_] -> Runnable
+                          end,
+              Tsks2 = gb_trees:enter(WId, WInfo#task_info{depends = UDeps}, Tsks),
+              {Runnable2, Tsks2}
+      end, {[], Tasks}, Waiters).
---
+++ master/src/pipeline_utils.erl
@@ -0,0 +1,216 @@
+-module(pipeline_utils).
+
+-include("common_types.hrl").
+-include("disco.hrl").
+-include("pipeline.hrl").
+
+-export([job_from_jobinfo/1, job_schedule_option/1]).
+-export([next_stage/2, group_outputs/2, pick_local_host/1]).
+-export([locations/1, ranked_locations/1]).
+
+% Compatibility utility: construct a pipeline from a job packet of
+% Disco 0.4.2 or earlier.
+
+-spec job_from_jobinfo(jobinfo()) -> {[task_output()],
+                                      pipeline() | unsupported_job}.
+job_from_jobinfo(#jobinfo{inputs = JI, map = M, reduce = R, nr_reduce = NR}) ->
+    job_from_jobinfo(JI, M, R, NR).
+
+job_from_jobinfo(JobInputs, Map, Reduce, Nr_reduce) ->
+    Inputs = task_inputs(JobInputs),
+    Pipeline = pipeline(Map, Reduce, Nr_reduce),
+    {Inputs, Pipeline}.
+
+-spec task_inputs([url() | [url()]]) -> [task_output()].
+task_inputs(Inputs) ->
+    % Currently, we assume that all pipeline inputs are data file
+    % inputs; dir-files as job inputs in the job pack are not
+    % supported.
+    [{N, {data, {0, 0, input_replicas(I)}}} || {N, I} <- disco:enum(Inputs)].
+
+-spec input_replicas([url() | [url()]]) -> [data_replica()].
+input_replicas(Input) when is_binary(Input) ->
+    % Single replica
+    [{Input, disco:preferred_host(Input)}];
+input_replicas(Reps) when is_list(Reps) ->
+    % Replica set
+    [{R, disco:preferred_host(R)} || R <- Reps].
+
+-spec pipeline(boolean(), boolean(), non_neg_integer())
+              -> pipeline() | unsupported_job.
+pipeline(false, false, _NR) ->
+    [];
+pipeline(false, true, 1) ->
+    [{?REDUCE, join_all}];
+pipeline(false, true, _NR) ->
+    % This was used to support reduce-only jobs with partitioned
+    % inputs from dir:// files. This is now unsupported; instead, the
+    % job-submitter will need to prepare an explicit pipeline.
+    unsupported_job;
+pipeline(true, false, _NR) ->
+    [{?MAP, split}, {?MAP_SHUFFLE, join_node}];
+pipeline(true, true, 1) ->
+    [{?MAP, split}, {?MAP_SHUFFLE, join_node}, {?REDUCE, join_all}];
+pipeline(true, true, _NR) ->
+    % This was used to support a pre-determined number of partitions
+    % in map output, which determined the number of reduces.  However,
+    % we now determine the number of reduces dynamically.
+    [{?MAP, split}, {?MAP_SHUFFLE, join_node},
+     {?REDUCE, join_label}, {?REDUCE_SHUFFLE, join_node}].
+
+-spec job_schedule_option(jobinfo()) -> task_schedule().
+job_schedule_option(#jobinfo{force_local = Local, force_remote = Remote}) ->
+    job_schedule_option(Local, Remote).
+% Prefer Local if both Local and Remote are set.
+job_schedule_option(true, _) -> local;
+job_schedule_option(_, true) -> remote;
+job_schedule_option(_, _) -> none.
+
+
+% Other utilities.
+
+-spec locations(data_input()) -> [host()].
+locations({data, {_Label, _Size, Replicas}}) ->
+    [H || {_Url, H} <- Replicas, H =/= none];
+locations({dir, {Host, _Url, _Labels}}) ->
+    [Host].
+
+% We don't currently rank by label in dir inputs.
+-spec ranked_locations(data_input()) -> [{data_size(), host()}].
+ranked_locations({data, {_Label, Size, Replicas}}) ->
+    [{Size, H} || {_Url, H} <- Replicas, H =/= none];
+ranked_locations({dir, {Host, _Url, Labels}}) ->
+    Size = lists:sum([Sz || {_L, Sz} <- Labels]),
+    [{Size, Host}].
+
+% When a task needs to consume node-local data, but a specific
+% data-unit has replicas, then we need to pick ad-hoc _a_ node to
+% which that data-unit is "local".  This just means that sometimes,
+% due to faults, a task could be re-run on another host, in which case
+% it would fetch its "local" input from a remote node.
+-spec pick_local_host([data_replica()]) -> url_host().
+pick_local_host(Replicas) ->
+    Reps = [Host || {_Url, Host} <- Replicas, Host =/= none],
+    case Reps of
+        [] -> none;
+        _ -> disco_util:choose_random(Reps)
+    end.
+
+-spec next_stage(pipeline(), stage_name()) -> stage() | done.
+next_stage([], _S) -> done;
+next_stage([{_S, _G} = First | _], ?INPUT) -> First;
+next_stage([{S, _G}], S) -> done;
+next_stage([{S, _G}, Next|_], S) -> Next;
+next_stage([_S|Rest], S) -> next_stage(Rest, S).
+
+% Dir files are a scalability mechanism to handle large numbers of
+% output labels (and hence output files) from a task.  When output
+% data is spread across a large number of labels by a large number of
+% stage tasks, usually each labeled file tends to be small.  Handling
+% each of (label, filename) as a separate output can take up a large
+% amount of memory in the master, and also can cause a huge number of
+% http requests to fetch these large number of small files over the
+% network.  Instead, the dir file approach puts all the (label, file)
+% tuples into a single 'dir' file, and sends the same of this file as
+% the output.  In order to perform label-based grouping, the labels in
+% the 'dir' file are also sent along with its name in the output spec.
+% A subsequent 'shuffle' stage using a join_node grouping normally
+% uses these 'dir' file inputs to compress the large number of small
+% files into fewer larger files per-label.
+
+% However, the notion of dir files creates a slightly complicates the
+% notion of label-based grouping.  For example, when the master
+% performs a 'split' grouping, which starts a task per stage input,
+% and a dir file is a stage input, it is not clear how many tasks need
+% to be started.  In such cases, we use a label as a unit; in the
+% 'split' case, it means we start a task per label.
+
+% utility to create a group -> [dir_spec()] mapping for the dir files
+% in the specified outputs.
+-spec dirdict([{task_id(), [task_output()]}]) -> dict().
+dirdict(Outputs) ->
+    Dirs = [{{L, H}, {{Tid, Outid}, D}}
+            || {Tid, Tout} <- Outputs,
+               {Outid, {dir, {H, _U, Labels}} = D} <- Tout,
+               Labels =/= [],
+               {L, _Sz} <- Labels],
+    lists:foldl(fun([], DirD) ->
+                        DirD;
+                   ([{G, _}|_] = HD, DirD) ->
+                        {_, DirList} = lists:unzip(HD),
+                        dict:store(G, DirList, DirD)
+                end, dict:new(), disco_util:groupby(1, lists:sort(Dirs))).
+
+-spec group_outputs(label_grouping(), [{task_id(), [task_output()]}])
+                   -> [grouped_output()].
+group_outputs(split, Outputs) ->
+    % As explained above, each label from a dir file is a separate
+    % 'split' output.
+    Dirs = [{{L, H}, [{{Tid, Outid}, D}]}
+            || {Tid, Tout} <- Outputs,
+               {Outid, {dir, {H, _U, Labels}} = D} <- Tout,
+               Labels =/= [],
+               {L, _Sz} <- Labels],
+    Dats = [{{L, pick_local_host(Reps)}, [{{Tid, Outid}, D}]}
+            || {Tid, Tout} <- Outputs,
+               {Outid, {data, {L, _S, Reps}} = D} <- Tout,
+               Reps =/= []],
+    Dirs ++ Dats;
+
+group_outputs(join_node_label, Outputs) ->
+    Dats = [{{L, pick_local_host(Reps)}, {{Tid, Outid}, D}}
+            || {Tid, Tout} <- Outputs,
+               {Outid, {data, {L, _S, Reps}} = D} <- Tout,
+               Reps =/= []],
+    DatGroups = disco_util:groupby(1, lists:sort(Dats)),
+    DirDict = dirdict(Outputs),
+    Groups = lists:foldl(fun([], Acc) ->
+                                 Acc;
+                            ([{G, _D}|_] = DG, Acc) ->
+                                 {_GList, DGroup} = lists:unzip(DG),
+                                 Group = case dict:find(G, DirDict) of
+                                             {ok, Dirs} -> Dirs ++ DGroup;
+                                             error -> DGroup
+                                         end,
+                                 [{G, Group} | Acc]
+                         end, [], DatGroups),
+    % Add in any dir-groups that were not coalesced with dat-groups.
+    GroupNames = lists:usort([G || {G, _Group} <- Groups]),
+    DirGroups = [DG || {G, _DG} = DG <- dict:to_list(DirDict),
+                       not lists:member(G, GroupNames)],
+    DirGroups ++ Groups;
+
+group_outputs(join_label, Outputs) ->
+    Dirs = [{L, {{Tid, Outid}, D}}
+            || {Tid, Tout} <- Outputs,
+               {Outid, {dir, {_H, _U, Labels}} = D} <- Tout,
+               Labels =/= [],
+               {L, _Sz} <- Labels],
+    Dats = [{L, {{Tid, Outid}, D}}
+            || {Tid, Tout} <- Outputs,
+               {Outid, {data, {L, _S, Reps}} = D} <- Tout,
+               Reps =/= []],
+    [{{L, none}, Group} % 'none' indicates no host is assigned by grouping.
+     || [{L,_D}|_] = LGroup <- disco_util:groupby(1, lists:sort(Dats ++ Dirs)),
+        {_L, Group} <- [lists:unzip(LGroup)]];
+
+group_outputs(join_node, Outputs) ->
+    Dirs = [{H, {{Tid, Outid}, D}}
+            || {Tid, Tout} <- Outputs,
+               {Outid, {dir, {H, _U, Labels}} = D} <- Tout,
+               Labels =/= []],
+    Dats = [{pick_local_host(Reps), {{Tid, Outid}, D}}
+            || {Tid, Tout} <- Outputs,
+               {Outid, {data, {_L, _S, Reps}} = D} <- Tout,
+               Reps =/= []],
+    [{{0, H}, Group}    % '0' indicates no label is assigned by grouping.
+     || [{H, _D}|_] = HGroup <- disco_util:groupby(1, lists:sort(Dats ++ Dirs)),
+        {_H, Group} <- [lists:unzip(HGroup)]];
+
+group_outputs(join_all, Outputs) ->
+    Dirs = [{{Tid, Outid}, D} || {Tid, Tout} <- Outputs,
+                                 {Outid, {dir, {_H, _U, _Labels}} = D} <- Tout],
+    Dats = [{{Tid, Outid}, D} || {Tid, Tout} <- Outputs,
+                                 {Outid, {data, {_L, _S, Reps}} = D} <- Tout,
+                                 Reps =/= []],
+    [{{0, none}, Dirs ++ Dats}].  % '0', 'none' indicate no assigned host or label.
