f8d75ca121ae69ca94bf013c63d975bbbf36fd91
Shayan Pooya <shayan@chango.com>
2014-04-29 16:44:42 -0400

    test: add some tests to document the behaviour of group_outputs function.

---
+++ master/test/pipeline_utils_test.erl
@@ -0,0 +1,48 @@
+-module(pipeline_utils_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+data_split_test() ->
+    DataSpec = {data, {5, 0, [{"out", "http://output"}]}},
+    Outputs = [{11, [{10, DataSpec}]}],
+    GOutputs = pipeline_utils:group_outputs(split, Outputs),
+    GOutputs = [{{5,"http://output"},[{{11,10}, DataSpec}]}].
+
+dir_split_test() ->
+    DirSpec = {dir, {"localhost", "localhost", [{9, "out"}]}},
+    Outputs = [{11, [{10, DirSpec}]}],
+    GOutputs = pipeline_utils:group_outputs(split, Outputs),
+    GOutputs = [{{9,"localhost"}, [{{11,10},DirSpec}]}].
+
+dir_data_split_test() ->
+    DirSpec = {dir, {"localhost", "localhost", [{9, "out"}]}},
+    DataSpec = {data, {0, 0, [{"other", "http://otherhost"}]}},
+    Outputs = [{11, [{10, DirSpec}, {12, DataSpec}]}],
+    GOutputs = pipeline_utils:group_outputs(split, Outputs),
+    GOutputs = [{{9,"localhost"}, [{{11,10},DirSpec}]},
+           {{0,"http://otherhost"},[{{11,12},DataSpec}]}].
+
+data_node_label_test() ->
+    DataSpec = {data, {5, 0, [{"out", "http://output"}]}},
+    Outputs = [{11, [{10, DataSpec}]}],
+    GOutputs = pipeline_utils:group_outputs(group_node_label, Outputs),
+    GOutputs = [{{5,"http://output"},[{{11,10}, DataSpec}]}].
+
+data_node_label_multi_test() ->
+    DataSpec1 = {data, {5, 600, [{"out1", "http://output1"}]}},
+    DataSpec2 = {data, {5, 700, [{"out2", "http://output2"}]}},
+    Outputs = [{11, [{10, DataSpec1}, {10, DataSpec2}]}],
+    GOutputs = pipeline_utils:group_outputs(group_node_label, Outputs),
+    Elem1 = {{5,"http://output1"}, [{{11,10},DataSpec1}]},
+    Elem2 = {{5,"http://output2"}, [{{11,10},DataSpec2}]},
+    case GOutputs of
+        [Elem1,Elem2] -> ok;
+        [Elem2,Elem1] -> ok
+    end.
+
+data_label_multi_test() ->
+    DataSpec1 = {data, {5, 600, [{"out1", "http://output1"}]}},
+    DataSpec2 = {data, {5, 700, [{"out2", "http://output2"}]}},
+    Outputs = [{11, [{10, DataSpec1}, {10, DataSpec2}]}],
+    GOutputs = pipeline_utils:group_outputs(group_label, Outputs),
+    GOutputs = [{{5,none}, [{{11,10},DataSpec1}, {{11,10},DataSpec2}]}].
